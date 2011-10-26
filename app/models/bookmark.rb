6498331e0fb9d08808d78183a8e9229f2f5f95d4
Ryan Waldron <rew@erebor.com>
2011-10-26 14:30:23 -0500

    Add .find and #save for Bookmark model.  Add spec and VCR cassette for same.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -9,6 +9,42 @@ class Bookmark
     end
   end
 
+  def attributes(*args)
+    array = args
+    array = self.instance_variables.map { |e| e.to_s.gsub("@", "").to_sym} if array == []
+    attrs = {}
+    array.each do |var|
+      attrs[var] = instance_variable_get("@#{var}")
+    end
+    attrs
+  end
+
+  attr_reader :errors
+
+  def self.find(id)
+    args = ["bookmarks/view"]
+    args << {id: id}
+    data = YvApi.get(*args) do |errors|
+      Rails.logger.info "API Error: Bookmark.find(#{id}) got these errors: #{errors.inspect}"
+      if errors.include? "not_found"
+        # return empty hash to avoid raising exception
+        { }
+      end
+    end
+    # If we got valid data back from API, use it; otherwise return nil.
+    Bookmark.new(data) if data.is_a? Hashie::Mash
+  end
+
+  def save
+    opts = attributes(:highlight_color, :labels, :reference, :title, :version)
+    opts.merge! ({auth_username: 'testuser', auth_password: 'tenders'})
+    response = YvApi.post('bookmarks/create', opts) do |errors|
+      @errors = errors.map { |e| e["error"] } if errors
+      return false
+    end
+    response
+  end
+
   def self.for_user(user_id = nil, params = {})
     key = user_id || 'default'
     page = params[:page] || 1
@@ -16,18 +52,16 @@ class Bookmark
     hash = {}
     api_data(user_id, params).bookmarks.each do |b|
       hash[b.id] = {
-        user_id: b.user_id,
-        version: b.version,
         created: b.created,
-        username: b.username,
-        labels: b.labels,
         highlight_color: b.highlight_color,
-        title: b.title,
+        labels: b.labels,
         reference: b.reference.osis,
-        labels: b.labels,
-        highlight_color: b.highlight_color,
         reference_chapter: b.reference_chapter,
-        references: b.references
+        references: b.references,
+        title: b.title,
+        user_id: b.user_id,
+        username: b.username,
+        version: b.version
       }
     end
     @@bookmarks["#{key}_#{page}"] = Hashie::Mash.new(hash)
---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Bookmark do
+  use_vcr_cassette "bookmark"
+  before :all do
+    @params = { email: "testuser23@youversion.com",
+               username: "testuser23",
+               password: "tenders",
+               agree: TRUE,
+               verified: TRUE,
+               locale: "en_US" }
+  end
+
+  describe ".save" do
+    it "returns true for saving valid params" do
+      # This is really brittle, because in the initial call (where we create our VCR cassette),
+      # this user has to be a valid user on the remote system. So that means we have to create
+      # the cassette with a new user, so that the first time, the user we expect is created.
+      # We could theoretically have a cassette somewhere with some real, existing users already
+      # in it, one that we knew existed on the remote system. But this would likewise be quite
+      # brittle. I don't have an answer, just making a note next time I'm through here and
+      # wondering why a spec that used to pass has started failing or something.
+      user = User.new(@params)
+      user.save.should_not be_false
+      bookmark = Bookmark.new({auth_username: user.username, auth_password: user.password, version: "esv",
+                               reference: "Matt.1", title: "Begettings", username: user.username})
+      bookmark.save.should_not be_false
+    end
+
+    it "returns false for invalid params" do
+      bad_bookmark = Bookmark.new({reference: "Matt.99"})
+      bad_bookmark.save.should be_false
+      bad_bookmark.errors.count.should == 1
+    end
+  end
+end
