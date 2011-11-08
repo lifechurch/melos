64c056220bec4f65a276922977520751ed575b7e
Caedmon Judd <caedmon@statebuilt.com>
2011-11-08 08:44:24 -0600

    Add Popular Notes widget

--- app/cells/likes_list_widget_cell.rb
+++ app/cells/likes_list_widget_cell.rb
@@ -5,7 +5,7 @@ class LikesListWidgetCell < Cell::Rails
     @likes = opts[:likes]
     @total = opts[:likes].count if opts[:likes]
     @title = opts[:title] ||= "Likes"
-    @link = opts[:link] ||= likes_path(opts[:user_id])
+    @link = opts[:link] ||= user_likes_path(opts[:user_id])
     render
   end
 
