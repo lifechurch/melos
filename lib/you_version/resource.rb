6298ce9a3362ed71ad9c6cfd18ea5c4a351eaef6
Development user <develop@li395-125.members.linode.com>
2011-11-09 14:41:59 -0500

    Little bit of refactoring and debug stuff

--- lib/you_version/resource.rb
+++ lib/you_version/resource.rb
@@ -12,28 +12,64 @@ module YouVersion
     include ActiveModel::Conversion
     
     class <<self
+      # This allows child class to easily override the prefix
+      # of its API path, if it happens to not be name.tableize.
+      def api_path_prefix
+        name.tableize
+      end
+
       def list_path
-        "#{name.tableize}/items"
+        "#{api_path_prefix}/items"
       end
 
       def resource_path
-        "#{name.tableize}/view"        
+        "#{api_path_prefix}/view"        
       end
       
       def update_path
-        "#{name.tableize}/update"
+        "#{api_path_prefix}/update"
       end
       
       def create_path
-        "#{name.tableize}/create"
+        "#{api_path_prefix}/create"
       end
       
       def delete_path
-        "#{name.tabelize}/delete"
+        "#{api_path_prefix}/delete"
       end
       
-      def find(id, auth = nil, params = {})        
-        response = get(resource_path, id: id ) do |e|   # anonymous    
+      def naughty_find(id, auth = nil, params = {}, &block)
+
+        bad_naughty = nil
+
+        # First try request as an anonymous request
+        response = get(resource_path, params.merge(id: id) ) do |e|
+          if (e.find {|t| t['key'] =~ /username_and_password.required/})
+            # If API said it wants authorization, try again with auth
+            get(resource_path, params.merge(id: id, auth: auth)) do |e|
+              # Capture errors
+              bad_naughty = e
+            end
+          else
+              # Capture errors
+            bad_naughty = e
+          end
+
+          # Down here we do something with the real error
+          if bad_naughty
+            if block_given?
+              response = block.call(response, bad_naughty)
+              # all the weird crap they do now
+            end
+            raise
+          end
+        end
+
+        new(response.merge(:auth => auth))
+      end
+      
+      def find(id, auth = nil, params = {})
+        response = get(resource_path, id: id ) do |e|   # anonymous
           get(resource_path, id: id, auth: auth) do |e| # auth'ed
             raise ResourceError.new(errors.map { |e| e["error"] })
           end
@@ -43,7 +79,7 @@ module YouVersion
       end
       
       def all(params = {})
-        response = YvApi.get(list_path) do |errors|
+        response = YvApi.get(list_path, params) do |errors|
           raise ResourceError.new(errors.map { |e| e["error"] })
         end
 
@@ -61,14 +97,16 @@ module YouVersion
       def destroy(id, auth = nil)
         @token = Digest::MD5.hexdigest "#{auth.username}.Yv6-#{auth.password}"
 
-        response = post(delete_path, {:id => id, :auth => auth})) do |errors|
+        response = post(delete_path, {:id => id, :auth => auth}) do |errors|
           raise ResourceError.new(errors.map { |e| e["error"] })
         end
         response
       end
       
       attr_accessor :resource_attributes
+
       def attribute(attr_name)
+        puts "Adding #{attr_name} to resource_attributes (which is a #{@resource_attributes.class})"
         @resource_attributes << attr_name
         
         define_method(attr_name) do
@@ -84,33 +122,35 @@ module YouVersion
         @secure_methods ||= []
         @secure_methods += secure_method_names.map(:to_sym)
       end
-      
-      def post(path, params)
+
+      def secure_caller?(caller)
         # TODO: Is this slow?  Nasty?  Probably. :(
         calling_method = caller.first.match(/`(.*)'$/)[1]
-        if @secure_methods.include?(calling_method.to_sym)
-          YvApi.post(path, params.merge(:secure => true))
-        else
-          YvApi.post(path)          
-        end
+        @secure_methods.include?(calling_method.to_sym)
+      end
+
+      # If the calling method is one of those for which
+      # we need to use https, add :secure => true to params
+      def securify(params, caller)
+        params.merge(:secure => true) if secure_caller?(caller)
+      end
+
+      def post(path, params, &block)
+        YvApi.post(path, securify(params, caller), &block)
       end
       
-      def get(path, params)
-        # TODO: Is this slow?  Nasty?  Probably. :(
-        calling_method = caller.first.match(/`(.*)'$/)[1]
-        if @secure_methods.include?(calling_method.to_sym)
-          YvApi.get(path, params.merge(:secure => true))
-        else
-          YvApi.get(path)          
-        end
+      def get(path, params, &block)
+        YvApi.get(path, securify(params, caller), &block)
       end
     end
 
     attr_accessor :attributes
-    
+
     attribute :id
     attribute :auth
-    
+
+    def after_build; end
+
     def initialize(data = {})
       @attributes = data
       after_build
