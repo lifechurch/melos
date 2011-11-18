8d1d973f1a822d53f8a97afe7f15d9d17fa1a507
Chad Bailey <chadbailey@gmail.com>
2011-11-18 13:45:00 -0600

    add'l server config
    Change-Id: I1bba665e4ab57ad5d9b1e08de9bc87d11863d32b

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,3 @@
-role :app, 'php1-san.youversion.com'
-role :db,  'php1-san.youversion.com', :primary => true
+role :app, 'php10-dfw.youversion.com'
+role :db,  'php10-dfw.youversion.com', :primary => true
 set :user, 'cbailey'
