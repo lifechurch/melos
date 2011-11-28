d53944b40fbd58c6ec0371a12e529ed8d1697052
Nathan Smith <nathan@sonspring.com>
2011-11-28 00:55:58 -0600

    Tweaks to make verse string builder class based (instead of ID).

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -465,16 +465,15 @@ var YV = (function($, window, document, undefined) {
 
         var button = $('#verses_selected_button');
         var count = $('#verses_selected_count');
-        var form = $('#verses_selected_form');
+        var input = $('.verses_selected_input');
         var flag = 'selected';
         var hide = 'hide';
 
         function parse_verses() {
           var total = $('.verse.' + flag).length;
-          var input = $('#verses_selected_input')[0];
 
           // Zero out value.
-          input.value = '';
+          input.val('');
 
           verse.each(function() {
             var el = $(this);
@@ -483,10 +482,10 @@ var YV = (function($, window, document, undefined) {
             if (el.hasClass(flag)) {
               if (input.value) {
                 // Add to hidden input.
-                input.value += '&' + this_id;
+                input.val(input.val() + '&' + this_id);
               }
               else {
-                input.value = this_id;
+                input.val(this_id);
               }
             }
           });
