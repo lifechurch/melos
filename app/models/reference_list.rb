120d4be582cc5ed2e3f950a24ea4cc7e2e8e501f
Ryan Waldron <rew@erebor.com>
2011-11-29 23:56:36 -0600

    Get rid of delegators; make << and concat work, initializing new References as needed; handle multi-verse (non-contiguous) references strings

--- app/models/reference_list.rb
+++ app/models/reference_list.rb
@@ -1,42 +1,72 @@
-require 'forwardable'
 class ReferenceList
   include Enumerable
-  extend Forwardable
-  def_delegators :@references, :<<, :each, :concat
 
-  attr_accessor :page, :total, :version
+  attr_accessor :total, :version
 
   def method_missing(method, *args, &block)
-    puts "Caller is #{caller.first}"
+    puts "In #{self.class}.method_missing, Caller is #{caller.first}, method is #{method}"
     result = @references.send(method, *args, &block)
     if result.is_a? Array
-      ReferenceList.new(*result)
+      ReferenceList.new(result)
     else
       result
     end
   end
 
+  # TODO: Fairly brittle - probably only going to work as written
+  # with just osis-able strings. Do we need << for this class?
+  # Probably not.
+  def <<(*args)
+    @references.concat args.map{|r| Reference.new(r.to_osis_string)}
+  end
+
   def initialize(args, version = nil)
-    puts "In initialize(#{args}  xxx  #{version})"
+    puts "In #{self.class}.initialize(#{args}  xxx  #{version})"
     self.version = version
     @references = []
 
     case args
     when Array
-      @references = args.map { |r| Reference.new("#{r.osis.downcase}.#{self.version}") }
-      @reference = args.map { |r| Reference.new("#{r.osis.downcase}.#{self.version}") }
+      # TODO: This needs to be more robust. In my limited experience so far, these
+      # arrays that arrive here are made up of scripture strings, meaning they don't
+      # have an .osis method.
+
+      # TODO: Refactor this and push most of it down into Reference. Eek, this is
+      # getting ugly.
+      @references = args.map do |r|
+        case r
+        when String
+          Reference.new("#{r.to_osis_string.downcase}.#{self.version}")
+        when Reference
+          r
+        when Hashie::Mash
+          Reference.new("#{r.osis.downcase}.#{self.version}")
+        end
+      end
     when String
-      @references = [Reference.new("#{args.downcase}.#{self.version}")]
+      @references = split_multi_ref_string(args.downcase)
     when Hashie::Mash
-      @references = [Reference.new("#{args.osis.downcase}.#{self.version}")]
+      @references = split_multi_ref_string(args.osis.downcase)
     end
 
-    # Get a default page value
-    @page = 1
     # Default to same as #size
     @total = self.count
   end
 
+  def split_multi_ref_string(ref_string)
+    ref_array = case ref_string
+    when /([\+\,])/  # if this case matches, either '+' or ',' will be stored in $1
+      ref_string.split($1)
+    else
+      # just stick the string in a single-element array so we only need to
+      # maintain one create-the-References line at the end
+      [ref_string]
+    end
+    ref_array.map{|str| Reference.new([str.strip, self.version].compact.join('.'))}
+  end
+
+  # Present our References in a string format suitable for sending to the API,
+  # ever-cognizant of the API version's requirements.
   def to_api_string
     join_str = case Cfg.api_version
     when "2.3"
