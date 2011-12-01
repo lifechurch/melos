f3735aff308648f2cc852a8538bc07a10f4702f1
Evan Prothro <Evan.Prothro@lifechurch.tv>
2011-11-30 19:40:35 -0600

    unstyled implementation of plans#show

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -9,51 +9,62 @@ class Plan < YouVersion::Resource
   attribute :name
   attribute :formatted_length
   attribute :copyright
-  
-  #attr_i18n_reader :name, :copyright
-
-  # @@all = {}
-  # 
-  #   def self.all(opts = {})
-  #     opts = {page: 1}.merge!(opts)
-  #     YvApi.get("reading_plans/library", page: opts[:page], category: opts[:category], language_tag: opts[:language]).reading_plans.map { |r| ReadingPlan.new(r.to_hash) }
-  #   end
-
-  def self.api_path_prefix
-    #name.tableize
-    "reading_plans"
-  end
 
   def self.categories(params = {})
     params = {page: 1}.merge!(params)
+    
     response = YvApi.get(list_path, params) do |errors|
       raise ResourceError.new(errors)
     end
     
     response.categories.children.map! {|c| c.label}
-    
   end
   
   def self.list_path
     "#{api_path_prefix}/library"
   end
   
+  def self.api_path_prefix
+    "reading_plans"
+  end
+  
+  def self.find(id, params = {}, &block)
+
+    if id.is_a?(String) 
+      #slug was passed, get id from slug with search, since API doesn't give a better way
+      lib_plan = search(id).find{|plan| plan.slug == id}
+      id = lib_plan.id
+    end
+
+    super(id, params, &block)
+    
+  end
+  def self.search(query, params = {})
+    
+    # Get search results for a query.
+    # 
+    #     URL: /2.3/reading_plans/search.json?query=parenting
+    #     Method: GET
+    # 
+    #     Parameters:
+    #     query of what you're wanting to search for
+    #     category  to filter reading plans to (optional)
+    #     total_days  will accept one of the predefined ranges to limit results to '1_day_to_7_days', '1_week_to_1_month', '1_month_to_3_months', '3_months_to_6_months', '6_months_to_1_year', '1_year_to_infinity', it's optional
+    #     language_tag  to filter reading plans to (optional, but highly recommended for best search results)
+    #     sort  the ordering of the results, defaults to 'score' (relevance), also accepts 'total_days'
+    #     page  number of results to return
+    
+    params = {query: query}.merge!(params)
+    
+    response = YvApi.get("#{api_path_prefix}/search", params) do |errors|
+      raise ResourceError.new(errors)
+    end
+    response.reading_plans.map {|data| new(data.merge(:auth => params[:auth]))}
+    
+  end
+  
   def to_param
     slug
   end
   
-  # def self.all(params = {})
-  #   params = {page: 1}.merge!(params)
-  #   response = YvApi.get(list_path, params) do |errors|
-  #     raise ResourceError.new(errors)
-  #   end
-  #   response.reading_plans
-  #   debugger
-  #   # puts "*"*80
-  #   # pp response
-  #   # puts "*"*80
-  #   # TODO: Switch to ResourceList here
-  #   response.send(api_path_prefix).map {|data| new(data.merge(:auth => params[:auth]))}
-  # end
-
 end
