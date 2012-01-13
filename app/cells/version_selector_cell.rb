c62195593269895f4e0e1d48b7915c0dcfa6f9a3
Evan Prothro <Evan.Prothro@lifechurch.tv>
2012-01-13 08:47:55 -0600

    reverting version selector cell code

--- app/cells/version_selector_cell.rb
+++ app/cells/version_selector_cell.rb
@@ -1,8 +1,17 @@
 class VersionSelectorCell < Cell::Rails
+  include ApplicationHelper
+  helper_method :bible_path
 
   def display(opts ={})
-    @reference = opts[:reference]
-    @versions  = Version.all_by_language
+    if opts[:reference]
+      @reference = opts[:reference]
+      @version = Version.find(@reference[:version])
+    elsif opts[:version]
+      @version = opts[:version]
+    end
+    @all_languages = Version.all_by_language
+    @this_language = @all_languages[@version.language.iso]
+    @all_languages = @all_languages.except(@version.language.iso)
     @languages = Version.languages
     render
   end
