c13a2635eeb3418beb0e197ff046bfa30e3df579
Chad Bailey <chadbailey@gmail.com>
2012-01-24 17:20:40 -0600

    trying a new api thing
    Change-Id: Ia282e4da56cbcee37394f048a8e78f1aa4262d5f

--- app/controllers/api_test_controller.rb
+++ app/controllers/api_test_controller.rb
@@ -4,8 +4,9 @@ class ApiTestController < ApplicationController
     @results = [{title: "gen.1.asv", data: time_call("bible/chapter", reference: "gen.1", version: "asv")},
                 {title: "gen.2.3.niv", data: time_call("bible/verse", reference: "gen.2.3", version: "niv")},
                 {title: "gen.5.msg", data: time_call("bible/chapter", reference: "gen.5", version: "msg")},
-                {title: "reading plans index", data: time_call("reading-plans/index")},
+                {title: "reading plans index", data: time_call("reading_plans/library")},
                 {title: "notes for genesis 1:1", data: time_call("notes/index", reference: "gen.1")},
+                {title: "404", data: time_call("foo/bar")}
                ]
   end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, 'ruby-1.9.2-p180@youversion-web'        # Or whatever env you want it to run in.
 require 'bundler/capistrano'
 
 
