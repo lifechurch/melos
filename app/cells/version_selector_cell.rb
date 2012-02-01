d83c2fb550143557a6df8e58b281acbff4da8b42
Evan Prothro <evan.prothro@lifechurch.tv>
2012-02-01 14:03:30 -0600

    closes #80

--- app/cells/version_selector_cell.rb
+++ app/cells/version_selector_cell.rb
@@ -6,9 +6,12 @@ class VersionSelectorCell < Cell::Rails
     if opts[:reference]
       @reference = opts[:reference]
       @version = Version.find(@reference[:version])
+      @alt_version = (!opts[:alt_referemce] || opts[:alt_referemce][:version] == @reference[:verson]) ? @version : Version.find(opts[:reference][:version]) #optimized for common case of no alt_version
     elsif opts[:version]
       @version = opts[:version]
+      @alt_version = @version
     end
+    
     @all_languages = Version.all_by_language
     @this_language = @all_languages[@version.language.iso]
     @all_languages = @all_languages.except(@version.language.iso)
