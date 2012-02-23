2bdedac102ba343fb745d8844f6e35a2b3d8e563
Evan Prothro <evan.prothro@lifechurch.tv>
2012-02-23 12:19:46 -0600

    fixed plan search and changed cache key creation. closes #167

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -61,8 +61,8 @@ class Plan < YouVersion::Resource
     #     sort  the ordering of the results, defaults to 'score' (relevance), also accepts 'total_days'
     #     page  number of results to return
     query = '*' if (query == "" || query == nil)
-    params = {query: query, cache_for: 12.hours}.merge!(params)
-    
+    params = {query: query, cache_for: 12.hours}.merge!(params.except("query", :query))
+
     response = YvApi.get("#{api_path_prefix}/search", params) do |errors|
       if errors.length == 1 && [/^No(.*)found$/, /^(.*)s not found$/].detect { |r| r.match(errors.first["error"]) }
         return []
