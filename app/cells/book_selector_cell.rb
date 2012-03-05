4f285df1e385bafdf4ddb6e54c2f0997036fb90f
Chad Bailey <chadbailey@gmail.com>
2012-03-05 00:37:40 -0600

    trying this for book chapter menu caching
    Change-Id: I13fab66db31a2b99b0911c807b96479c5cc4d972

--- app/cells/book_selector_cell.rb
+++ app/cells/book_selector_cell.rb
@@ -1,6 +1,9 @@
 class BookSelectorCell < Cell::Rails
 include ApplicationHelper
 helper_method :bible_path
+  cache :display do |cell, opts|
+    "#{@reference}_#{@version}"
+  end
 
   def display(opts = {})
     @reference = opts[:reference]
