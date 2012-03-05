592333d48dace5957f1bb9f14a11ce3012778490
Chad Bailey <chadbailey@gmail.com>
2012-03-05 16:36:41 -0600

    changing mobile redirect
    Change-Id: Ic4ca8acc23e110fab97d72f778b11ad443ed8a26

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module YouversionWeb
       puts "env mobile device is #{ENV["X_MOBILE_DEVICE"]}"
       # r301 /.*/,  Proc.new {|path, rack_env| "http://#{rack_env['SERVER_NAME'].gsub(/fr\./i, '') }/fr#{path}" },
       #   :if => Proc.new {|rack_env| rack_env['SERVER_NAME'] =~ /fr\./i}
-      r301 /.*/,  Proc.new {|path, rack_env| "http://m.#{rack_env['SERVER_NAME']}#{path}" },
+      r301 /.*/,  Proc.new {|path, rack_env| "http://#{rack_env['SERVER_NAME'].gsub(/www/, "m")}#{path}" },
         :if => Proc.new { |rack_env| !rack_env["X_MOBILE_DEVICE"].nil? }
 
       ### BIBLE REDIRECTS
