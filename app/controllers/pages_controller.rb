499a4d5a7c874377afbdb44194ce2d84040e6194
Chad Bailey <chadbailey@gmail.com>
2012-03-07 11:25:32 -0600

    working on authorize
    Change-Id: I8056ba18b7ad27ada8d1f66fb365293beb15b67a

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def donate
     # @donate_link = Bloodhound.bleed_american?(request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip) ? us_donation_path : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=P87AYS9RLXTEE"
-    @donate_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=P87AYS9RLXTEE"
+    # @donate_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=P87AYS9RLXTEE"
+    @donate_link = us_donation_path
   end
 
   def l10n; end
