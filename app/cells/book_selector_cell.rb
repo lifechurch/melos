59560bf96b7c378378b83d4758ae3ef6226e973a
Evan Prothro <evan.prothro@lifechurch.tv>
2012-03-15 15:35:06 -0500

    adding locale to cache for book selector

--- app/cells/book_selector_cell.rb
+++ app/cells/book_selector_cell.rb
@@ -2,7 +2,7 @@ class BookSelectorCell < Cell::Rails
 include ApplicationHelper
 helper_method :bible_path
   cache :display, :expires_in => 6.hours do |cell, opts|
-    opts[:version]
+    "#{opts[:version]}_#{I18n.locale}"
   end
 
   def display(opts = {})
