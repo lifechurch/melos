1664da65a249344af9106030092dcfb46372c376
Chad Bailey <chadbailey@gmail.com>
2012-03-16 13:57:56 -0500

    fixed non-detecting button
    Change-Id: I4af445689fcada6be460b2b477710b05dbc7fd79

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def donate
-    @donate_link = Bloodhound.bleed_american?(request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip) ? us_donation_path : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=P87AYS9RLXTEE"
+    @us_donate_link = us_donation_path
+    @intl_donate_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=P87AYS9RLXTEE"
   end
 
   def l10n; end
