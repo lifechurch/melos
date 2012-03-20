18cd92343f1f0b15b4b49a5bb4daa1c1dd1fec9e
Evan Prothro <evan.prothro@lifechurch.tv>
2012-03-20 15:11:54 -0500

    omfg, class variable with storage per user.

--- app/models/bookmark_list.rb
+++ app/models/bookmark_list.rb
@@ -1,6 +1,6 @@
 class BookmarkList
-  @@api_data = {}
-  @@bookmarks = {}
+  @api_data = {}
+  @bookmarks = {}
 
   def total(user_id)
     api_data[user_id].total
@@ -19,7 +19,7 @@ class BookmarkList
       targ = Bookmark.new(b)
       hash[b.id] = targ
     end
-    @@bookmarks["#{key}_#{page}"] = Hashie::Mash.new(hash)
+    @bookmarks["#{key}_#{page}"] = Hashie::Mash.new(hash)
   end
 
   # TODO: Memoizing this is likely expensive and unnecessary. Need to cogitate some
@@ -28,14 +28,14 @@ class BookmarkList
     page = params[:page] || 1
     key = "#{user_id || 'default'}_#{page}"
 
-    unless @@api_data.has_key?(key)
+    unless @api_data.has_key?(key)
       args = ["bookmarks/items"]
       opts = {page: page}
       opts.merge!(user_id: user_id) if user_id
       args << opts
-      @@api_data[key] = YvApi.get(*args)
+      @api_data[key] = YvApi.get(*args)
     end
-    @@api_data[key]
+    @api_data[key]
   end
 
   def api_data(user_id = nil)
