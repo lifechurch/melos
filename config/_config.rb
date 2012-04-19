94be7fff214a59c587857a4767eee1dd66cd7c5d
Kevin Morey <kevin.morey@gmail.com>
2012-04-18 23:09:16 -0500

    Update donate page
    - move form submit buttons to left align for consistency
    - add address fields
    - style tweaks to form (per Andy)
    - add US states list to config
    - fix layout bug in donate landing page.  some i18n were breaking layout.

--- config/_config.rb
+++ config/_config.rb
@@ -4,14 +4,16 @@
 # specific confgurations via `Cfg.value`.
 #
 
-class Cfg 
+class Cfg
   cattr_reader :languages
-  @@settings = YAML::load_file(File.expand_path('../_config.yml', __FILE__))[Rails.env] 
+  cattr_reader :usstates
+  @@settings = YAML::load_file(File.expand_path('../_config.yml', __FILE__))[Rails.env]
   @@languages = YAML::load_file(File.expand_path('../languages.yml', __FILE__))
-  class MissingConfigOptionError < StandardError; end 
-  def self.method_missing(key, *arguments) 
-    raise MissingConfigOptionError("#{key.to_s} is not in the config file") unless @@settings.include?(key.to_s) 
-    @@settings[key.to_s] 
-  end 
+  @@usstates = YAML::load_file(File.expand_path('../usstates.yml', __FILE__))
+  class MissingConfigOptionError < StandardError; end
+  def self.method_missing(key, *arguments)
+    raise MissingConfigOptionError("#{key.to_s} is not in the config file") unless @@settings.include?(key.to_s)
+    @@settings[key.to_s]
+  end
 end
 
