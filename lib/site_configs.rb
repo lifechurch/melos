3f3f48c60d82a8e27546ba7699948d978ae09cd8
Chad Bailey <chadbailey@gmail.com>
2012-04-19 11:31:14 -0500

    removed comment
    Change-Id: I571a635bc35e018f4f2c8d54ebc12f71937a7998

--- lib/site_configs.rb
+++ lib/site_configs.rb
@@ -4,7 +4,6 @@ module SiteConfigs
     hash = Hash.new(SiteConfigs::Site)
 
     # Define white-label classes here.
-    # These are strings to prevent rails from barfing on the class names
     hash["biblesocietywebsite.org"] = SiteConfigs::ElSalvador
     hash["biblesociety.co.za"] = SiteConfigs::Sabs
     hash["lvh.me"] = SiteConfigs::Sabs
