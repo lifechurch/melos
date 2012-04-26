16abc3f757425efafc28de78b09982004ede9af8
Evan Prothro <evan.prothro@lifechurch.tv>
2012-04-26 13:06:54 -0500

    loading us states in helper and using options_for_select form helper

--- config/_config.rb
+++ config/_config.rb
@@ -6,10 +6,8 @@
 
 class Cfg
   cattr_reader :languages
-  cattr_reader :usstates
   @@settings = YAML::load_file(File.expand_path('../_config.yml', __FILE__))[Rails.env]
   @@languages = YAML::load_file(File.expand_path('../languages.yml', __FILE__))
-  @@usstates = YAML::load_file(File.expand_path('../usstates.yml', __FILE__))
   class MissingConfigOptionError < StandardError; end
   def self.method_missing(key, *arguments)
     raise MissingConfigOptionError("#{key.to_s} is not in the config file") unless @@settings.include?(key.to_s)
