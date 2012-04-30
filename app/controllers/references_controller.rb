3e86776a9d093f10caa792085939ae5e7c716866
Evan Prothro <evan.prothro@lifechurch.tv>
2012-04-30 16:21:16 -0500

    quickfix to fix bug where /sk doesn't load a version?

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -14,6 +14,7 @@ class ReferencesController < ApplicationController
     # Redirect if it's anything other than book.chapter.version, ignoring verses for now
     if !params[:reference]
       flash.keep
+      debugger
       return redirect_to bible_path(last_read || Reference.new(book: "john", chapter: "1", version: current_version))
       #TODO: What if gen.1 doesn't exist for current_version? OK with 'this doesn't exist, select another' view?
     else
