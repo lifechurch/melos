cb12682fe04df49ad1ca7a38bb7db6151db83d17
Evan Prothro <evan.prothro@lifechurch.tv>
2012-05-03 10:24:11 -0500

    initial code

--- lib/site_configs/site.rb
+++ lib/site_configs/site.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module SiteConfigs
 
   class Site
@@ -15,9 +16,19 @@ module SiteConfigs
     
     def versions; end
     
+    def partners #this static content will eventually come from the API, putting it here for now as white-labels could have issue
+      {:en      => [{name: 'Publisher 1', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}, 
+                    {name: 'Publisher 2', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}, 
+                    {name: 'Publisher 3', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}, 
+                    {name: 'Publisher 4', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}, 
+                    {name: 'Publisher 5', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}],
+       :"pt-BR" => [{name: 'casa editora 1', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}],
+       :"zh-CN" => [{name: '鸣谢我们的合作伙伴 1', url: 'http://www.publisherville.com', image: 'Biblica-540x320.png'}]}
+    end
+    
     def to_s
       self.class.name.gsub('SiteConfigs::', '')
     end
-
+    
   end
 end
