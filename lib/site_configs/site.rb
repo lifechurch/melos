dc028b51d0d211cfd74377765cb6d12fe8717684
Evan Prothro <evan.prothro@lifechurch.tv>
2012-05-07 15:45:55 -0500

    serving site logo from s3

--- lib/site_configs/site.rb
+++ lib/site_configs/site.rb
@@ -2,7 +2,7 @@ module SiteConfigs
 
   class Site
     def logo_style
-      "background: url(/assets/yv_logo.png) no-repeat 0 14px; width: 150px"
+      "background: url(yv_logo.png) no-repeat 0 14px; width: 150px"
     end
 
     def default_version; end
