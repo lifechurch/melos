3e437d17493eeda8046cea69cdba230806db5eb8
Evan Prothro <evan.prothro@lifechurch.tv>
2012-05-11 11:39:02 -0500

    secure assets from S3

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,14 +47,7 @@ YouversionWeb::Application.configure do
     # Generate digests for assets URLs
     config.assets.digest = true
     
-    config.action_controller.asset_host = Proc.new do |*args|
-      source, request = args
-      if request.try(:ssl?)
-        "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-      else
-        "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-      end
-    end
+    config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
     
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     config.assets.precompile += %w( ie7.css ie8.css ie9.css wysiwyg/jquery.wysiwyg.css wysiwyg/jquery.wysiwyg.js wysiwyg/editor.css donate.css status.css )
