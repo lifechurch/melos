ac382540fec480e2fbfe48c228644c76e13c4c09
Doug Blauser <dsblauser@hotmail.com>
2012-07-14 14:49:10 -0500

    Added comment for previously commented line

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -64,7 +64,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id], auth: current_auth)
 
     if @bookmark.update(params[:bookmark])
-#      render action: "show"
+#      render action: "show" - commented out by DSB 7/14/12
       render action: "index"
     else
       render action: "edit"
