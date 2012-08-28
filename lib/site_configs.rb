23b7dea2a70c480a3d1980becab519672e2e2098
Evan Prothro <evan.prothro@lifechurch.tv>
2012-08-28 11:56:46 -0500

    update config for dev and staging

--- lib/site_configs.rb
+++ lib/site_configs.rb
@@ -8,6 +8,7 @@ module SiteConfigs
     hash["biblesociety.co.za"] = SiteConfigs::Sabs
     hash["bible.com"] = SiteConfigs::Bible
     hash["dev.bible.com"] = SiteConfigs::Bible
+    hash["staging.bible.com"] = SiteConfigs::Bible
     hash["lvh.me"] = SiteConfigs::Bible
     hash
   end
