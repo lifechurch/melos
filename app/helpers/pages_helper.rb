be3a590c1e9bbf91e188f98e45cea5c29469e59a
Evan Prothro <evan.prothro@lifechurch.tv>
2012-08-31 11:42:31 -0500

    i18n for 404 messages

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,7 @@
 module PagesHelper
 
   def page_not_found_messages
-    [
-      "That page is so lost, only Jesus could find it.",
-      "Book of Errors 4:04 - 'And it was said unto them, that page was not found.'",
-      "Seek and ye shall find -- except for this page. It's missing.",
-      "Sorry, this page may have been raptured -- we couldn't find it."
-    ]
+    (1..4).map{|i| t("app errors.404 message #{i}")}
   end
 
 end
