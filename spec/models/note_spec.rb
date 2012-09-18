abf055dd8fddf3b791c6fe9a1b901afd7b3c13a5
Evan Prothro <evan.prothro@lifechurch.tv>
2012-09-18 12:16:58 -0500

    added test

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -12,7 +12,7 @@ describe Note do
     @valid_attributes = {title: "My New Note", content: "Some Content", reference: "gen.2.1-3.kjv,gen.3.1-5.kjv", user_status: "public", auth: @auth }
   end
 
-  
+
   describe ".save" do
     it 'creates a new note and returns the correct response' do
       @note = Note.new( title: "My New Note", content: "<p>Some Content</p>", reference: "gen.1.1-3.1-kjv", auth: @auth )
@@ -22,7 +22,7 @@ describe Note do
       @note.reference_list.should be_a ReferenceList
       @note.reference_list.first.should be_a Reference
     end
-    
+
     it 'creates a note with no references' do
       @note = Note.new( title: "My New Note", content: "<p>Some Content</p>", auth: @auth ).save.should be_true
     end
@@ -33,7 +33,7 @@ describe Note do
       @valid_note = Note.new(@valid_attributes)
       result = @valid_note.save
       result.should be_true
-      @note = Note.find(@valid_note.id, @auth) 
+      @note = Note.find(@valid_note.id, @auth)
       @note.title.should == 'My New Note'
       @note.content.should == 'Some Content'
       @note.version.should be_a(Version)
@@ -44,9 +44,9 @@ describe Note do
 
     it 'returns false if Note was not found' do
       lambda { Note.find('0', @auth) }.should  raise_error(YouVersion::ResourceError)
-    end        
+    end
   end
-  
+
   describe ".all" do
     it 'returns true after finding all Notes for the user' do
       @note = Note.new( title: "My New Note", content: "Some Content", reference: "gen.1.1-3.kjv", auth: @auth )
@@ -93,13 +93,17 @@ describe Note do
   end
 
   describe "for reference" do
-    it "should return notes for a reference" do
-      @note = Note.new( title: "My New Note", content: "Some Content", reference: "gen.2.1.kjv", auth: @auth )
-      @note.save
-      # FIXME
-      # HACK HACK HACK
-      # TODO: This shouldn't need a user id!
-      notes = Note.for_reference(Reference.new("gen.2.1.kjv"), user_id: @auth.user_id, auth: @auth)
+    before :all do
+      note = Note.new( title: "My New Note", content: "Some Content", reference: "gen.2.1.kjv", auth: @auth )
+      note.save
+    end
+    it "should return result for a single verse reference" do
+      notes = Note.for_reference(Reference.new("gen.2.1.kjv"))
+      notes.should be_a ResourceList
+      notes.first.should be_a Note
+    end
+    it "should return results for a chapter reference" do
+      notes = Note.for_reference(Reference.new("gen.2.kjv"))
       notes.should be_a ResourceList
       notes.first.should be_a Note
     end
