a63fb46c39c2392c1fe9d5639520d862b7299ca6
Evan Prothro <evan.prothro@lifechurch.tv>
2012-11-09 20:39:12 -0600

    added ability to press enter to select first search match:

--- app/assets/javascripts/menus/version.js
+++ app/assets/javascripts/menus/version.js
@@ -4,6 +4,7 @@ function VersionMenu( opts ) {
   this.alt_version  = opts.alt_version || false;
   this.search_input = this.menu.find('.search input');
   this.searchTimer  = null;
+  this.firstMatch    = 'first-match';
 
   this.initLinks();  // Copied directly from previous code.  Needs refactoring.
   this.initSearch();
@@ -92,22 +93,38 @@ VersionMenu.prototype = {
   filterVersions : function (filter) {
     if(filter.length){
       var regexp = new RegExp(filter, 'i');
-      // hide all rows
-      this.menu.find("tr").hide();
       var category = null;
+      var thiss = this;
+
+      // hide all rows and remove previous first-match
+      this.menu.find("tr").hide();
+      this.menu.find("tr." + this.firstMatch).removeClass(this.firstMatch);
+      var noMatch = true;
+
+
       // show matching versions and their category headers
       $.each(this.menu.find("tr[data-meta]"), function() {
         if($(this).attr('data-meta').match(regexp)){
+          // show matching version row
           $(this).show();
+
+          // give first match hover style
+          if (noMatch){
+            $(this).addClass(thiss.firstMatch);
+            noMatch = false;
+          }
+
           // show closest language header
-          // TODO: avoid firing this prevAll if already shown! CPU ouch.
-          category = $(this).prev('tr.cat');
+          category = $(this).prev('tr.cat'); //if immediately previous sibling
           if (category.length == 0){
+            // if more than one sibling back -- avoiding use of prevAll() inefficiently
             category = $(this).prevUntil('tr.cat').last().prev();
           }
           category.show();
         }
       });
+      //TODO: highlight first result via css
+      //.dynamic_menu .browse.searching li:first(visible)
       //TODO: fire hover event so user knows what would select if enter is pressed
     }
     else{
@@ -121,19 +138,28 @@ VersionMenu.prototype = {
     //on keyup, start the countdown
     var thiss = this;
 
-    this.search_input.keyup(function(){     // search as user types
-      clearTimeout(thiss.searchTimer);
-      thiss.searchTimer = setTimeout(thiss.filterVersions(thiss.search_input.val()), 350);
-    });
-
-    this.trigger.click(function(){          // focus search on menu open
-      setTimeout(function() {               // we have to wait because it takes a bit
-        thiss.search_input.focus();         // to show the menu (?!)
+    this.trigger.click(function(){                        // focus search on menu open
+      setTimeout(function() {                             // we have to wait because it takes a bit
+        thiss.search_input.focus();                       // to show the menu (?!)
       }, 100);
     });
 
     this.search_input.focus( function(){
       $(this).select();
+    });
+
+    this.search_input.keyup(function(){                   // search as user types
+      clearTimeout(thiss.searchTimer);
+      thiss.searchTimer = setTimeout(thiss.filterVersions(thiss.search_input.val()), 350);
+    });
+
+    this.search_input.keyup(function(e){                  // enter selects first match
+      if(e.keyCode == 13) {
+        var firstMatchLink = thiss.menu.find('.' + thiss.firstMatch + ' a');
+        if (firstMatchLink.length){
+          firstMatchLink.click();
+        }
+      }
     });
   }
 
