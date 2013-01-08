adee5e81216f5b8629c7e84eeb78f9cf22de4273
Evan Prothro <evan.prothro@lifechurch.tv>
2013-01-08 14:25:24 -0600

    security fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# Disable the default XML params parser - its full of holes
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
