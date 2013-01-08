92d8cebb66c9d493bd70f91ac538a58dd71e9ebc
Evan Prothro <evan.prothro@lifechurch.tv>
2013-01-08 14:45:06 -0600

    securty fix that actually works, you know.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-# Disable the default XML params parser - its full of holes
-ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+# Disable the default XML params parser - its full of holes
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
 
 # Initialize the rails application
 YouversionWeb::Application.initialize!
