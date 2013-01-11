81372e378138994d172d1271df1aafc6efc1895a
Evan Prothro <evan.prothro@lifechurch.tv>
2013-01-11 09:56:32 -0600

    twitter and facebook separate counters

--- app/assets/javascripts/panes/bookmark.js
+++ app/assets/javascripts/panes/bookmark.js
@@ -10,7 +10,6 @@ BookmarkPane.prototype = {
 
   updateForm : function( data ) {
     var refs = data.references || [];
-    console.log(data);
     this.references_field.val(refs.join(",")); // populate hidden field
   }
 }
