88f3972c0f6db0f0d35d8c54531d00d40722d19d
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-01-23 19:38:15 -0700

    Find user now accounts for not logged in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -421,7 +421,7 @@ private
   def find_user
     user_id   = params[:user_id] || params[:id]
     @user     = (user_id.to_s.downcase == current_user.try(:username).to_s.downcase) ? current_user : User.find(user_id)
-    @me       = true if (@user.id == current_user.id)
+    @me       = true if (@user.id == current_user.try(:id))
     self.sidebar_presenter = Presenter::Sidebar::User.new(@user,params,self)
   end
 
