7e0fc7a0eb64fe9f2bd9b62a6876edbb3c9a9c64
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-01-25 11:24:57 -0700

    Handle NoSecondaryVersion for subscriptions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -320,34 +320,39 @@ class ApplicationController < ActionController::Base
     @current_version = Version.find(@current_version).to_param rescue Version.default
   end
 
+  # TODO: Refactor alt version functionality
   def alt_version(ref)
     #Cookie may have empty string for some reason -- possibly previous error case
     cookies[:alt_version] = nil if cookies[:alt_version].blank?
 
     if cookies[:alt_version].present?
-      begin
-        # validate that the preferred secondary version has the reference in question
-        includes_ref = Version.find(cookies[:alt_version]).include?(ref)
 
+      # validate that the preferred secondary version has the reference in question
+      begin
+        Version.find(cookies[:alt_version]).include?(ref)
       # Catch versions that don't exist and raise proper error
       rescue NotAVersionError
         cookies[:alt_version] = nil # nuke the bad cookie. BAD COOKIE!
         raise NoSecondaryVersionError
-
       # rescue anything else, nuke cookie
       rescue
         cookies[:alt_version] = nil # nuke the bad cookie. BAD COOKIE!
       end
     end
-
+    # if we have an alt_version at this point, let's return it.
     return cookies[:alt_version] if cookies[:alt_version].present?
 
     # new user or bad version was in cookie
+    # Find recent version that is not the current version and includes ref.
     recent = recent_versions.find{|v| v.to_param != current_version && v.include?(ref)}
     cookies[:alt_version] = recent.to_param if recent
+    # Nothing? Okay let's call sample_for with locale, version and validating ref
     cookies[:alt_version] ||= Version.sample_for((params[:locale] || "en"), except: current_version, has_ref: ref)
-
+    # Still nothing? Let's fall back to KJV.  This is secondary version that they can switch at any time.
+    cookies[:alt_version] ||= Version.find(1).id
+    # If were still blank, we've got problems
     raise NoSecondaryVersionError if cookies[:alt_version].blank?
+
     cookies[:alt_version]
   end
 
