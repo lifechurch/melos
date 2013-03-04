0927f483d32a67ab4e5a56d5fd1d580bb302ae2d
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-03-03 17:24:12 -0700

    Redirect to full absolute url.

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -2,7 +2,7 @@ class TrackingsController < ApplicationController
 
   def app
     Gabba::Gabba.new('UA-3571547-76','youversion.com').event("Bible.com", "navigate", "page", "/app", true)
-    redirect_to("/download")
+    redirect_to("http://www.youversion.com/download")
   end
 
 end
