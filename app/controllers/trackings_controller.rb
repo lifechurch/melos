58ca7a300b4071af456720a7ba3b6bb0e45e32f5
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-05-07 12:55:11 -0600

    GA event tracking for /app and removing mongo tracking.

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,21 +1,9 @@
 class TrackingsController < ApplicationController
 
   def app
-    db = MongoConnector.connection
-    collection = db.collection("app")
-
-    datetime    = DateTime.now.change(sec:0).utc  # time object with minute resolution
-    seconds     = datetime.to_i                   # changes to epoch
-
-    resolution  = {'sec' => seconds}
-    insert      = {'$inc' => {"cnt" => 1 }}
-
-    collection.update(resolution, insert, {:upsert  => true}) # fire and forget
-
-
     tracker = Gabba::Gabba.new(@site.ga_code, @site.ga_domain)
     tracker.identify_user(cookies[:__utma], cookies[:__utmz])
-    tracker.event("App", "Download")
+    tracker.event("App Download", "#{request.host_with_port}#{request.fullpath}")
 
     redirect_to("/download", status: 302)
   end
