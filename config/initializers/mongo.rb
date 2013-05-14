bc65e9ad62e5719ed28d1ff29efef11fe0c56589
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-05-14 13:11:35 -0600

    Update to new Memcache service on Heroku (old one is unsupported), remove mongo tracking support code, upgrade dalli gem.

--- config/initializers/mongo.rb
+++
@@ -1,6 +0,0 @@
-MongoConnector.connection_info = {
-  database: "BibleSeries",
-  replicas: ["candidate.1.mongolayer.com:10008","candidate.2.mongolayer.com:10008"],
-  password: "yYfu824CWq7qTy0enjg9w5uYqw27U",
-  user: "yv-web"
-}
