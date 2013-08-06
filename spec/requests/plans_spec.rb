6bcea3cf0202068a1c8138cf267aab13ed005c70
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-08-06 11:18:07 -0500

    Fix spec helper sign-in, update and add a few more plan/subscription specs.

--- spec/requests/plans_spec.rb
+++ spec/requests/plans_spec.rb
@@ -1,34 +1,37 @@
 require 'spec_helper'
 
-describe "Requesting Reading Plans" do
+describe "Reading Plans" do
 
   before do
     @base_url = "http://www.example.com"
   end
 
-  describe "the index page (plans#index)" do
-
-    it "should render corectly" do
-
-      get "/reading-plans"
-      response.code.should == "200"
-      response.should render_template(:index)
-
-    end
-
+  it "should show me a list of reading plans" do
+    visit "/reading-plans"
+    page.should have_content("Browse All Plans")
   end
 
-
-  describe "the show page (plans#show)" do
-
-    it "should render corectly" do
-
-      get "/reading-plans/45-wisdom"
-      response.code.should == "200"
-      response.should render_template(:show)
-
-    end
-
+  it "should show me an individual reading plan" do
+    visit "/reading-plans/45-wisdom"
+    page.should have_content("Wisdom")
+    page.should have_content("More Info")
+    page.should have_content("View a Sample Reading")
+    page.should have_content("Publisher")
   end
+
+  it "should let me read samples of the reading plan" do
+    visit "/reading-plans/45-wisdom"
+    click_link("View a Sample Reading")
+
+    page.should have_content("Start This Plan")
+    page.should have_content("Day 1 of 12")
+    page.should have_content("Next Day")
+    page.should have_content("And Solomon made affinity with Pharaoh king of Egypt") #reader content
+
+    click_link("Next Day")
+    page.should have_content("Start This Plan")
+    page.should have_content("Day 2 of 12")
+  end
+
 
 end
