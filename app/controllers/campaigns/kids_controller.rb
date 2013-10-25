e94890df2bc8dc28aa16b987d7fe66ad1367a30a
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-10-25 15:46:16 -0500

    Skipping unnecessary before filters.

--- app/controllers/campaigns/kids_controller.rb
+++ app/controllers/campaigns/kids_controller.rb
@@ -1,6 +1,17 @@
 class Campaigns::KidsController < ApplicationController
+  
   layout 'kids'
+
+  # I'm sure there's a better way to do this, this works for now.
+  skip_before_filter :set_page
+  skip_before_filter :skip_home
+  skip_before_filter :check_facebook_cookie
+  skip_before_filter :tend_caches
+  skip_before_filter :set_default_sidebar
+
+
   caches_action :index
+
 
   def index
   end
