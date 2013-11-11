caf0b4c7f717c742a35d1b69b68e1d5af80de58c
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-11-11 12:24:47 -0600

    Initial basic VOD working.

--- app/models/vod.rb
+++ app/models/vod.rb
@@ -9,9 +9,38 @@ class VOD < YV::Resource
     end
 
 
-    def today
+    # Solid assumptions that can be made as discussed with Adam
+    # Verse of the day:
+    # - will always be in same book
+    # - will always be in same chapter
+    # - will always have continuous verses if there is more than one verse
+    def today(version_id = 1)
       today = Date.today.yday
-      all.detect {|d| d.day == today}
+      item = all.detect {|d| d.day == today} # {"references"=>["PHP.3.13+PHP.3.14"], "day"=>315}
+      
+      # join multiple items in array, then split on our + to get all reference pieces
+      references    = item.references.join("+").split("+")  # ["PHP.3.13","PHP.3.14"]
+      verse_nums    = references.collect {|ref| ref.split(".").last} # get 13 from PHP.3.13 for each ref occurence
+      pieces        = references.first.split(".")
+      usfm_chapter  = pieces.first + "." + pieces.second # "PHP.3"
+
+
+      opts = { id: version_id, reference: usfm_chapter }
+      data, errs = get("bible/chapter", opts)
+      results = YV::API::Results.new(data,errs)
+
+      selector = verse_nums.map{|v_num|".v#{v_num} .content"}.join(', ')
+      document = Nokogiri::HTML(results.content)
+      verse_text = document.css(selector).inner_html.strip
+
+      data = {
+        version: Version.find(version_id),
+        human: results.reference.human + ":" + verse_nums.join(", "),
+        usfm: results.reference.usfm,
+        verses: verse_nums,
+        content: verse_text
+      }
+      Hashie::Mash.new(data)
     end
 
 
