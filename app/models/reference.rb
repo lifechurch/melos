428e08265caa8bfb4ebb3e471378b4bf38fc5f61
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-11-13 13:03:34 -0600

    Support new reference.usfm array response.

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,5 +1,30 @@
 # encoding: UTF-8
 
+
+  # {"reference"=>{"version_id"=>1, "human"=>"Genesis 2", "usfm"=>["GEN.2"]},
+  #  "copyright"=>{"text"=>"Crown Copyright in UK", "html"=>"Crown Copyright in UK"},
+  #  "content"=>
+  #   "<div class=\"version vid1 iso6393eng\" data-vid=\"1\" data-iso6393=\"eng\">\n    <div class=\"book bkGEN\">\n        <div class=\"chapter ch2\" data-usfm=\"GEN.2\">\n            <div class=\"label\">2</div><div class=\"p\"><span class=\"verse v1\" data-usfm=\"GEN.2.1\"><span class=\"label\">1</span><span class=\"content\">Thus the heavens and the earth were finished, and all the host of them.</span></span></div><div class=\"p\"><span class=\"verse v2\" data-usfm=\"GEN.2.2\"><span class=\"label\">2</span><span class=\"content\">And on the seventh day God ended his work which he had made; and he rested on the seventh day from all his work which he had made.</span></span></div><div class=\"p\"><span class=\"verse v3\" data-usfm=\"GEN.2.3\"><span class=\"label\">3</span><span class=\"content\">And God blessed the seventh day, and sanctified it: because that in it he had rested from all his work which God created and made.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">created&#8230;: Heb. created to make</span></span></span></span></div><div class=\"p\"><span class=\"verse v4\" data-usfm=\"GEN.2.4\"><span class=\"label\">4</span><span class=\"content\">These are the generations of the heavens and of the earth when they were created, in the day that the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God made the earth and the heavens,</span></span></div><div class=\"p\"><span class=\"verse v5\" data-usfm=\"GEN.2.5\"><span class=\"label\">5</span><span class=\"content\">And every plant of the field before it was in the earth, and every herb of the field before it grew: for the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God had not caused it to rain upon the earth, and there was not a man to till the ground.</span></span></div><div class=\"p\"><span class=\"verse v6\" data-usfm=\"GEN.2.6\"><span class=\"label\">6</span><span class=\"content\">But there went up a mist from the earth, and watered the whole face of the ground.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">there&#8230;: or, a mist which went up from, etc.</span></span></span></span></div><div class=\"p\"><span class=\"verse v7\" data-usfm=\"GEN.2.7\"><span class=\"label\">7</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God formed man of the dust of the ground, and breathed into his nostrils the breath of life; and man became a living soul.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">of the dust&#8230;: Heb. dust of the ground</span></span></span></span></div><div class=\"p\"><span class=\"verse v8\" data-usfm=\"GEN.2.8\"><span class=\"label\">8</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God planted a garden eastward in Eden; and there he put the man whom he had formed.</span></span></div><div class=\"p\"><span class=\"verse v9\" data-usfm=\"GEN.2.9\"><span class=\"label\">9</span><span class=\"content\">And out of the ground made the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God to grow every tree that is pleasant to the sight, and good for food; the tree of life also in the midst of the garden, and the tree of knowledge of good and evil.</span></span></div><div class=\"p\"><span class=\"verse v10\" data-usfm=\"GEN.2.10\"><span class=\"label\">10</span><span class=\"content\">And a river went out of Eden to water the garden; and from thence it was parted, and became into four heads.</span></span></div><div class=\"p\"><span class=\"verse v11\" data-usfm=\"GEN.2.11\"><span class=\"label\">11</span><span class=\"content\">The name of the first is Pison: that is it which compasseth the whole land of Havilah, where there is gold;</span></span></div><div class=\"p\"><span class=\"verse v12\" data-usfm=\"GEN.2.12\"><span class=\"label\">12</span><span class=\"content\">And the gold of that land is good: there is bdellium and the onyx stone.</span></span></div><div class=\"p\"><span class=\"verse v13\" data-usfm=\"GEN.2.13\"><span class=\"label\">13</span><span class=\"content\">And the name of the second river is Gihon: the same is it that compasseth the whole land of Ethiopia.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">Ethiopia: Heb. Cush</span></span></span></span></div><div class=\"p\"><span class=\"verse v14\" data-usfm=\"GEN.2.14\"><span class=\"label\">14</span><span class=\"content\">And the name of the third river is Hiddekel: that is it which goeth toward the east of Assyria. And the fourth river is Euphrates.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">toward&#8230;: or, eastward to Assyria</span></span></span></span></div><div class=\"p\"><span class=\"verse v15\" data-usfm=\"GEN.2.15\"><span class=\"label\">15</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God took the man, and put him into the garden of Eden to dress it and to keep it.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">the man: or, Adam</span></span></span></span></div><div class=\"p\"><span class=\"verse v16\" data-usfm=\"GEN.2.16\"><span class=\"label\">16</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God commanded the man, saying, Of every tree of the garden thou mayest freely eat:</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">thou&#8230;: Heb. eating thou shalt eat</span></span></span></span></div><div class=\"p\"><span class=\"verse v17\" data-usfm=\"GEN.2.17\"><span class=\"label\">17</span><span class=\"content\">But of the tree of the knowledge of good and evil, thou shalt not eat of it: for in the day that thou eatest thereof thou shalt surely die.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">thou shalt surely&#8230;: Heb. dying thou shalt die</span></span></span></span></div><div class=\"p\"><span class=\"verse v18\" data-usfm=\"GEN.2.18\"><span class=\"label\">18</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God said, It is not good that the man should be alone; I will make him an help meet for him.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">meet&#8230;: Heb. as before him</span></span></span></span></div><div class=\"p\"><span class=\"verse v19\" data-usfm=\"GEN.2.19\"><span class=\"label\">19</span><span class=\"content\">And out of the ground the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God formed every beast of the field, and every fowl of the air; and brought them unto Adam to see what he would call them: and whatsoever Adam called every living creature, that was the name thereof.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">Adam: or, the man</span></span></span></span></div><div class=\"p\"><span class=\"verse v20\" data-usfm=\"GEN.2.20\"><span class=\"label\">20</span><span class=\"content\">And Adam gave names to all cattle, and to the fowl of the air, and to every beast of the field; but for Adam there was not found an help meet for him.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">gave: Heb. called</span></span></span></span></div><div class=\"p\"><span class=\"verse v21\" data-usfm=\"GEN.2.21\"><span class=\"label\">21</span><span class=\"content\">And the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God caused a deep sleep to fall upon Adam, and he slept: and he took one of his ribs, and closed up the flesh instead thereof;</span></span></div><div class=\"p\"><span class=\"verse v22\" data-usfm=\"GEN.2.22\"><span class=\"label\">22</span><span class=\"content\">And the rib, which the </span><span class=\"nd\"><span class=\"content\">Lord</span></span><span class=\"content\"> God had taken from man, made he a woman, and brought her unto the man.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">made: Heb. builded</span></span></span></span></div><div class=\"p\"><span class=\"verse v23\" data-usfm=\"GEN.2.23\"><span class=\"label\">23</span><span class=\"content\">And Adam said, This is now bone of my bones, and flesh of my flesh: she shall be called Woman, because she was taken out of Man.</span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">Woman: Heb. Isha</span></span></span><span class=\"note f\"><span class=\"label\">#</span><span class=\" body\"><span class=\"ft\">Man: Heb. Ish</span></span></span></span></div><div class=\"p\"><span class=\"verse v24\" data-usfm=\"GEN.2.24\"><span class=\"label\">24</span><span class=\"content\">Therefore shall a man leave his father and his mother, and shall cleave unto his wife: and they shall be one flesh.</span></span></div><div class=\"p\"><span class=\"verse v25\" data-usfm=\"GEN.2.25\"><span class=\"label\">25</span><span class=\"content\">And they were both naked, the man and his wife, and were not ashamed.</span></span></div>\n        </div>\n    </div>\n</div>",
+  #  "audio"=>
+  #   [{"id"=>8,
+  #     "version_id"=>1,
+  #     "title"=>"KJV Listener's Bible",
+  #     "download_urls"=>
+  #      {"format_mp3_32k"=>
+  #        "//static-youversionapi-com.commondatastorage.googleapis.com/bible/audio/8/32k/GEN/2-e7404531a3e84e887f8d3b86ce7bcba1.mp3?version_id=1"}}],
+  #  "previous"=>
+  #   {"toc"=>true,
+  #    "canonical"=>true,
+  #    "version_id"=>1,
+  #    "human"=>"Genesis 1",
+  #    "usfm"=>["GEN.1"]},
+  #  "next"=>
+  #   {"toc"=>true,
+  #    "canonical"=>true,
+  #    "version_id"=>1,
+  #    "human"=>"Genesis 3",
+  #    "usfm"=>["GEN.3"]}}
+
 class Reference < YV::Resource
 
   attr_reader :book
@@ -36,7 +61,7 @@ class Reference < YV::Resource
       when Reference
         ref.to_hash
 
-      when Hash#, Hashie::Mash (is mash creation even used/necessary?)
+      when Hash
         ref
           
       else
@@ -94,19 +119,64 @@ class Reference < YV::Resource
     end
   end
 
-
   # Human readable version of the reference
   # Ex: 
   #   Genesis 1:2 for GEN.1.2 reference
   #   Genesis 1:2-4 for GEN.1 with verses 2-4
 
   def human
-      return attributes.reference.human.to_s + ":#{verses.first}" if single_verse?
-      return attributes.reference.human.to_s if is_chapter?
-      # we only support references being consecutive ranges
-      # so at this point we can assume it is
-      attributes.reference.human.to_s + ":#{verses.first.to_s}" + "-#{verses.last.to_s}"
+    human = attributes.reference.human
+    return "#{human}:#{verses.first}" if single_verse?
+    return human if is_chapter?
+    # we only support references being consecutive ranges
+    # so at this point we can assume it is
+    return "#{human}:#{verses.first.to_s}-#{verses.last.to_s}"
   end
+
+
+  # Get a chapter reference (all verses) from current reference
+  #
+  # example:
+  #   reference => Genesis 1:1
+  #   reference.to_chapter => Genesis 1
+  #
+  # returns:
+  #   an instance of Reference
+  #
+  def to_chapter
+    Reference.new(self, verses: nil)
+  end
+
+  # Reference for a previous chapter for a Reference instance
+  def previous_chapter
+    return nil unless version
+    return nil unless attributes.previous.present?
+    Reference.new(attributes.previous.usfm.first, version: version)
+  end
+
+  # Reference for the next chapter for a Reference instance
+  def next_chapter
+    return nil unless version
+    return nil unless attributes.next.present?
+    Reference.new(attributes.next.usfm.first, version: version)
+  end
+
+  # Determine if reference instance is a chapter reference
+  def is_chapter?
+    @is_chapter ||= (verses.empty? || false)
+  end
+
+  # Determine if reference instance is for a single verse
+  def single_verse?
+    verses.count == 1 rescue false
+  end
+
+  #HACK: looking for "intro" is not a great way to check if a chapter is
+  #actually an intro, but it will do for now
+  def is_intro?
+    !self.usfm.match(/\.intro/i).nil?
+  end
+
 
   # In some legitimate cases, the reference is valid in some versions, but
   # maybe not the current version. In such cases we want to display a "best
@@ -127,28 +197,20 @@ class Reference < YV::Resource
     end
   end
 
-  # returns a String for a relative deep link path to be used for app urls.
-  # ex: youversion://reference=REF&version_id=VER
-  def deep_link_path
-    return "bible?reference=#{chapter_usfm}&version_id=#{version}" if version
-    return "bible?reference=#{chapter_usfm}"
+
+
+
+  # A usfm representation scoped to just book / chapter for a reference
+  # Ex: GEN.3 for Genesis 3 | Genesis 3:1
+  # returns a string
+  def chapter_usfm    
+    @chapter_usfm ||= "#{book}.#{chapter}" #memoizing because of chapter_list indexing
   end
 
-  # Get a chapter reference (all verses) from current reference
-  #
-  # example:
-  #   reference => Genesis 1:1
-  #   reference.to_chapter => Genesis 1
-  #
-  # returns:
-  #   an instance of Reference
-  #
-  def to_chapter
-    self.class.new(self, verses: nil)
-  end
 
   # Return proper usfm format for reference instance
-  # Ex: GEN.3.4 for Genesis 3:4
+  # Ex: GEN.3.4                   for Genesis 3:4
+  #     GEN.1.1+GEN.1.2+GEN.1.3   for Genesis 1:1-3
 
   def to_usfm
     return "#{chapter_usfm}" if is_chapter?
@@ -166,27 +228,24 @@ class Reference < YV::Resource
     return human
   end
 
-  # A usfm representation scoped to just book / chapter for a reference
-  # Ex: GEN.3 for Genesis 3 | Genesis 3:1
-  # returns a string
-  def chapter_usfm
-    #memoizing because of chapter_list indexing
-    @chapter_usfm ||= "#{book}.#{chapter}"
-  end
 
   # A string format for the reference Version
   # Ex: "KJV" if version id = 1
   # returns a string
   def version_string
-    Version.find(version).human if version
+    if version
+      @version_string ||= Version.find(version).human
+    end
   end
+
 
   def to_param
     _ref = "#{to_usfm}" if is_chapter? || single_verse?
     _ref ||= "#{chapter_usfm}.#{verses.first}-#{verses.last}"
-    _ver = ".#{Version.find(version).abbreviation}" if version
-    "#{_ref}#{_ver}".downcase
+    _ver = "#{Version.find(version).abbreviation}" if version
+    "#{_ref}.#{_ver}".downcase
   end
+
 
   def [](arg)
     return self.try(arg.to_sym)
@@ -200,7 +259,6 @@ class Reference < YV::Resource
   end
 
   def ==(compare)
-    #if same class
     (compare.class == self.class) &&  compare.hash == hash
   end
 
@@ -208,16 +266,15 @@ class Reference < YV::Resource
     self == compare
   end
 
-  def notes
-    @notes ||= Note.for_reference(self)
-  end
-
   def merge(hash)
     Reference.new(to_hash.merge(hash))
   end
 
-  def single_verse?
-    verses.count == 1 rescue false
+  # returns a String for a relative deep link path to be used for app urls.
+  # ex: youversion://reference=REF&version_id=VER
+  def deep_link_path
+    return "bible?reference=#{chapter_usfm}&version_id=#{version}" if version
+    return "bible?reference=#{chapter_usfm}"
   end
 
   def short_link
@@ -226,7 +283,16 @@ class Reference < YV::Resource
 
   def copyright
     self.class.i18nize(attributes.copyright) if version
+  end  
+
+  def valid?
+    return content != ""
   end
+
+  def to_hash
+    {book: book, chapter: chapter, verses: verses, version: version}
+  end
+
 
 
   # API Method
@@ -285,42 +351,7 @@ class Reference < YV::Resource
     return @audio
   end
 
-  def previous_chapter
-    return nil unless version
-    return nil if attributes.previous.blank?
 
-    self.class.new(attributes.previous.usfm, version: version)
-  end
-
-  def next_chapter
-    return nil unless version
-    return nil if attributes.next.blank?
-
-    self.class.new(attributes.next.usfm, version: version)
-  end
-
-  def is_chapter?
-    @is_chapter ||= (verses.empty? || false)
-  end
-
-  #HACK: looking for "intro" is not a great way to check if a chapter is
-  #actually an intro, but it will do for now
-  def is_intro?
-    !self.usfm.match(/\.intro/i).nil?
-  end
-
-  def valid?
-    return content != ""
-  end
-
-  def osis
-    Rails.logger.apc "#{self.class}##{__method__} is deprecated,use the 'to_param' or 'to_usfm' methods instead", :debug
-    to_param
-  end
-
-  def to_hash
-    {book: book, chapter: chapter, verses: verses, version: version}
-  end
 
 
 
@@ -328,7 +359,6 @@ class Reference < YV::Resource
 
   def content_document
     @content_document ||= Nokogiri::HTML(attributes.content)
-    #TODO: #PERF: cache this with memcache if we keep and use it
   end
 
   # API Method
