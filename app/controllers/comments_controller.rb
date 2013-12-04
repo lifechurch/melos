5fdf2fbc225e6f4c17466ba0bbb64fcc63d1a119
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-12-04 10:39:50 -0600

    Authorization on comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
 
+  before_filter :force_login
+
   def create
     @comment  = Comment.create({
       moment_id:  params[:comment][:moment_id].to_i,
       content:    params[:comment][:content],
       auth:       current_auth})
     
-    if @comment.valid?
-       redirect_to(:back, notice: "Saved")
-    else
-       redirect_to(:back, notice: "Error")
-    end
+    notice = @comment.valid? ? "Saved" : "Error"
+    redirect_to(:back, notice: notice)
   end
 
 end
