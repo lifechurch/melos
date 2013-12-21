6b223887685d015ae6f3152bd04bc50bc2df4888
Britt Mileshosky <britt.mileshosky@lifechurch.tv>
2013-12-21 00:53:20 -0600

    Redirection concerns for app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   
   include YV::Concerns::UserAuth
+  include YV::Concerns::Redirection
+
   include ApplicationHelper
   protect_from_forgery
   
-  helper_method :sidebar_presenter, :presenter, :client_settings, :follow_redirect, :redirect_path, :clear_redirect, :recent_versions, :set_cookie, :find_user, :current_date, :last_read, :current_version, :alt_version, :bible_path, :a_very_short_time, :a_short_time, :a_long_time, :a_very_long_time, :bdc_user?
+  helper_method :sidebar_presenter, :presenter, :client_settings, :recent_versions, :set_cookie, :find_user, :current_date, :last_read, :current_version, :alt_version, :bible_path, :a_very_short_time, :a_short_time, :a_long_time, :a_very_long_time, :bdc_user?
   before_filter :set_page
   before_filter :set_site
   before_filter :set_locale_and_timezone
@@ -211,42 +213,6 @@ class ApplicationController < ActionController::Base
 
   def api_record_not_found(ex)
     render_404
-  end
-
-  # set redirect for (to) argument
-  # otherwise set redirect location to a redirect param if available
-  def set_redirect(to = nil)
-    clear_redirect if cookies[:auth_redirect] == "" #EVENTUALLY: understand why this cookie is "" instaed of nil/dead, to avoid this workaround
-    cookies[:auth_redirect] = to unless to.nil?
-    cookies[:auth_redirect] ||= params[:redirect] if params[:redirect]
-  end
-
-  def redirect_path
-    cookies[:auth_redirect]
-  end
-
-  def clear_redirect
-    cookies[:auth_redirect] = nil
-  end
-
-  def next_redirect?( to )
-    redirect_path == to
-  end
-
-
-  def follow_redirect(opts = {})
-    cookie_path = cookies[:auth_redirect].to_s == '' ? nil : cookies[:auth_redirect] #EVENTUALLY: understand why this cookie is "" instaed of nil/dead, to avoid this workaround
-    clear_redirect
-    path = cookie_path || opts[:alt_path] || bible_path
-    return redirect_to path, notice: opts[:notice] if opts[:notice]
-    return redirect_to path, error: opts[:error] if opts[:error]
-    return redirect_to path
-  end
-
-  def external_request?
-    return true  if request.referer.blank?
-    return false if request.host == request.referer.split('/')[2].split(':')[0]
-    return true
   end
 
   def recent_versions
