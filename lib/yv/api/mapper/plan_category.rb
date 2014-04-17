5ab4668106e30283bab63e11d9201a331300e45c
Matt Anderson <matthewrossanderson@gmail.com>
2014-04-17 10:03:00 -0400

    Localize reading plan list and categories

--- lib/yv/api/mapper/plan_category.rb
+++ lib/yv/api/mapper/plan_category.rb
@@ -37,11 +37,29 @@ module YV
             return pc
           end
 
+          def locale
+            # This is a hack until the API team decides what's going on with locales
+            case I18n.locale.to_s
+            when 'pt-BR'
+              'pt'
+            when 'en-GB'
+              'en_GB'
+            when 'es-ES'
+              'es'
+            when 'zh-CN'
+              'zh_CN'
+            when 'zh-TW'
+              'zh_TW'
+            else
+              I18n.locale.to_s
+            end
+          end
+
           def to_plan_categories(children)
             children.collect do |data|
               p = ::PlanCategory.new
               p.slug = data.category
-              p.label = data.labels[I18n.locale.to_s]
+              p.label = data.labels[locale]
               p
             end
           end
