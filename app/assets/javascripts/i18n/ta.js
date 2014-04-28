5907103d1b1b8abfc85311dfdc5594705e4b78d7
Russ Calhoun <russ.calhoun@lifechurch.tv>
2014-04-28 09:00:50 -0500

    adding localize js json files

---
+++ app/assets/javascripts/i18n/ta.js
@@ -0,0 +1,2 @@
+I18n.translations || (I18n.translations = {});
+I18n.translations["ta"] = {"ui":{"pagination":{"next page":"அடுத்த பக்கம்","previous page":"முந்தின பக்கம்"}},"moments":{"action":{"like":"விரும்பு","share":"பகிர்","edit":"திருத்து"}}};
