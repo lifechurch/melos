cd9f4b4bf895ccf104a807cb79736dd44e2837be
Russ Calhoun <russ.calhoun@lifechurch.tv>
2014-05-13 09:17:25 -0500

    javascript i18n js files generated

--- app/assets/javascripts/i18n/hr.js
+++ app/assets/javascripts/i18n/hr.js
@@ -1,2 +1,2 @@
 I18n.translations || (I18n.translations = {});
-I18n.translations["hr"] = {"ui":{"pagination":{"next page":"Sljedeća strana","previous page":"Prethodna strana"},"datetime":{"just now":"Upravo sad"}},"moments":{"action":{"load more":"Učitaj još","like":"Sviđa mi se","liked":"Svidjelo se","share":"Podijeli","comment":"Komentar","edit":"Izmjeni","delete":"Obriši"}}};
+I18n.translations["hr"] = {"ui":{"yes button":"Da","no button":"Ne","pagination":{"next page":"Sljedeća strana","previous page":"Prethodna strana"},"datetime":{"just now":"Upravo sad"}},"moments":{"action":{"load more":"Učitaj još","like":"Sviđa mi se","liked":"Svidjelo se","share":"Podijeli","comment":"Komentar","edit":"Izmjeni","delete":"Obriši"}}};
