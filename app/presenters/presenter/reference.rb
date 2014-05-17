9a065ff4d4bd561e9410586373cfccc991dc8ca4
Matt Anderson <matthewrossanderson@gmail.com>
2014-05-17 14:20:48 -0400

    Hotfix for subscriptions in reader
    Fixes
    https://app.getsentry.com/youversion-web/youversion-web/group/19359625/

--- app/presenters/presenter/reference.rb
+++ app/presenters/presenter/reference.rb
@@ -3,13 +3,14 @@ module Presenter
 
     include Presenter::ReaderInterface
 
-    attr_accessor :reference, :alt_reference, :version, :alt_version
+    attr_accessor :reference, :alt_reference, :version, :alt_version, :subscriptions
     
     def initialize( ref_string, params = {}, controller = nil, opts = {})
       super(params,controller)
       @reference_string = ref_string
       @reference        = ::Reference.new(reference_hash)
       @version          = Version.find(reference.version)
+      @subscriptions    = ::Subscription.all(controller.send(:current_user))
       @alt_version      = opts[:alt_version]   || Version.find(controller.send(:alt_version,reference))
       @alt_reference    = opts[:alt_reference] || ::Reference.new(reference, version: alt_version)
     end
