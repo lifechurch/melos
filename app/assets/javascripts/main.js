1e29465ada4b046e34f3c65ae42e5fe5405386eb
Russ Calhoun <russ.calhoun@lifechurch.tv>
2014-06-04 12:01:36 -0500

    ensure fallback to english for lang w/ missing keys

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -126,6 +126,7 @@ jQuery(document).ready(function() {
 
 jQuery(window).load(function() {
   // init locale for js I18n-js
+  I18n.fallbacks = true;
   I18n.defaultLocale = $('html').data('default-locale');
   I18n.locale = $('html').data('locale');
 });
