f8d112c0a635d52a6689f4ff07a74bb8da4476e2
Matt Anderson <matthewrossanderson@gmail.com>
2014-07-10 22:54:30 -0400

    Vod subscribe on feeds

--- app/controllers/vod_subscriptions_controller.rb
+++ app/controllers/vod_subscriptions_controller.rb
@@ -2,7 +2,7 @@ class VodSubscriptionsController < ApplicationController
 
   layout 'settings'
 
-  before_filter :find_versions
+  before_filter :versions_for_current_language
   before_filter :find_vod_subscription
 
   def index
@@ -29,13 +29,6 @@ class VodSubscriptionsController < ApplicationController
 
   def find_vod_subscription
     @vod_subscription = VodSubscription.all(auth: current_user.auth)
-  end
-
-  def find_versions    
-    primary_locale = Version.find(params[:context_version]).language.tag rescue nil if params[:context_version].present?
-    primary_locale ||= I18n.locale.to_s
-    primary_locale = "en" if primary_locale == "en-GB" #TODO fix.this.hack
-    @versions_by_lang = Version.by_language({:only => primary_locale})
   end
 
 end
--- app/views/moments/_votd.json.jbuilder
+++ app/views/moments/_votd.json.jbuilder
@@ -20,6 +20,17 @@ json.object do
   end
   json.actions do
     json.read true
+    json.subscribable true if current_user
     # json.share true
+  end
+
+  if current_user
+    json.subscription do  
+      subscription =       VodSubscription.all(auth: current_user.auth)
+      json.path            user_vod_subscriptions_path(current_user.username)
+      json.time            subscription.email.time
+      json.version_id      subscription.email.version_id
+      json.versions        Version.by_language({:only => I18n.locale.to_s}).map{|v| { version_id: v.id, title: v.title } }
+    end
   end
 end
