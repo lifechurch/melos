bfe574802cee3b4e91851d6fd4c1e0f997fde984
Matt Anderson <matthewrossanderson@gmail.com>
2014-07-10 22:54:45 -0400

    JS Localization

--- app/assets/javascripts/i18n/ta.js
+++ app/assets/javascripts/i18n/ta.js
@@ -1,2 +1,2 @@
 I18n.translations || (I18n.translations = {});
-I18n.translations["ta"] = {"ui":{"yes button":"ஆம்","no button":"இல்லை","pagination":{"next page":"அடுத்த பக்கம்","previous page":"முந்தின பக்கம்"}},"moments":{"action":{"like":"விரும்பு","share":"பகிர்","comment":"கருத்து","edit":"திருத்து"}}};
+I18n.translations["ta"] = {"ui":{"yes button":"ஆம்","no button":"இல்லை","pagination":{"next page":"அடுத்த பக்கம்","previous page":"முந்தின பக்கம்"}},"moments":{"action":{"like":"விரும்பு","share":"பகிர்","comment":"கருத்து","edit":"திருத்து"}},"or":{"lower":"அல்லது","upper":"அல்லது"}};
