d7e210f352b4305e886ebebf38576e76cd323088
Russ Calhoun <russ.calhoun@lifechurch.tv>
2014-12-08 09:39:03 -0600

    YV-3072 show pt versions at the top of pt/versions, add cache for versions page, some minor tweaks

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -5,6 +5,7 @@ class VersionsController < ApplicationController
     primary_locale = Version.find(params[:context_version]).language.tag rescue nil if params[:context_version].present?
     primary_locale ||= I18n.locale.to_s
     primary_locale = "en" if primary_locale == "en-GB" #TODO fix.this.hack
+    primary_locale = "pt-BR" if primary_locale == "pt"
     cur_lang = Hash[primary_locale, @versions_by_lang.delete(primary_locale)]
     @versions_by_lang = cur_lang.merge(@versions_by_lang)
     self.sidebar_presenter = Presenter::Sidebar::Versions.new(@versions_by_lang,params,self)
