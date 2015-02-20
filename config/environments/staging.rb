1a7bb5a113702d618ce538f8e0e8ccc0938ad086
Michael Martin <michael.martin@lifechurch.tv>
2015-02-19 19:19:14 -0600

    Stop Compression of Assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -34,10 +34,10 @@ YouversionWeb::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
+  # config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
   
   # Do not compress assets
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
