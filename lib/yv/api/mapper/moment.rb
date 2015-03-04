3b81795b385efced9b4d92875c91a48b09f4e985
Matt Anderson <matthewrossanderson@gmail.com>
2015-03-04 15:39:20 -0500

    Hack to exclude image moments from Prod

--- lib/yv/api/mapper/moment.rb
+++ lib/yv/api/mapper/moment.rb
@@ -14,6 +14,8 @@ module YV
 
           def from_all(results)
             return results if results.empty? or results.moments.nil?
+            # Reject is temp hack
+            results.moment.reject! {|m| m.kind_id == "image.v2" }
             results.moments.collect do |moment_data|
               map_from_kind(moment_data)
             end 
