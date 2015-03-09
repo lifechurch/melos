9dd874f4b9b202355fa40e88ad84add1333e845f
Michael Martin <michael.martin@lifechurch.tv>
2015-03-09 14:33:45 -0500

    Clean up Console Output

--- deploy/cleanup.js
+++ deploy/cleanup.js
@@ -5,16 +5,14 @@ var Heroku = require('heroku-client'),
 	heroku = new Heroku({ token: process.env.HEROKU_API_KEY });
 
 heroku.apps().list(function(err, apps) {
-	//console.log(err, apps);
 	apps.forEach(function(app, i) {
-		//console.log(app.name, app.created_at, app.updated_at);
 
 		var now = new Date();
 		var lastDeploy = new Date(Date.parse(app.updated_at));
+		
 		console.log(app.name, daysOld(now, lastDeploy), "days old", "Is CI Build?", isCIBuild(app.name));
 
 		if (daysOld(now, lastDeploy) > 10 && isCIBuild(app.name)) {
-			console.log(app.name);
 			deleteApp(app.name);
 		}
 	});
@@ -32,7 +30,11 @@ function deleteApp(n) {
 	//Hard code name check as safety measure
 	if (n !== 'yv-prod' && n !== 'yv-staging' && n !== 'yv-dev' && n !== 'yv-node') { 
 		heroku.apps(n).delete(function(err, app) {
-			console.log(err, app);
+			if (err) { 
+				console.log(err);
+			} else {
+				console.log("App " + n + " deleted successfully.");
+			}
 		});
 	}
 }
