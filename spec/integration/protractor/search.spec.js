51cb8cf779dfc036370c96addd4a3e3d5c2e35e1
Michael Martin <michael.martin@lifechurch.tv>
2015-03-17 16:51:16 -0500

    Updating Last 3 Tests

--- spec/integration/protractor/search.spec.js
+++ spec/integration/protractor/search.spec.js
@@ -1,2 +1,22 @@
 describe('youversion search', function() {
+	var search_field = element(by.css("#header_search_input"));
+	var search_button = element(by.css("#header_search_button"));	
+	var search_header = element(by.css("#main article h1"));
+	var niv_option = element(by.css("#version_form select option[value='111']"));
+	var noresults_header = element(by.css("#noresults"));
+
+	beforeEach(function() {
+		browser.get('http://staging.bible.com/');
+	});
+
+	it('should search the bible', function() {
+		var search_phrase = "Fawns";
+		search_field.sendKeys(search_phrase);
+		search_button.click();
+		expect(search_header.getText()).toEqual("Search results for: " + search_phrase);
+
+		niv_option.click();
+		expect(noresults_header.isPresent()).toBeFalsy();
+	});
+	
 });
