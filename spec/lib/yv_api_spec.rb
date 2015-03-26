b7c62dc6dba4a6527a9f3564a37ca33cafd3d333
Matt Anderson <matthewrossanderson@gmail.com>
2015-03-26 13:56:19 -0400

    Kick it to the curb

--- spec/lib/yv_api_spec.rb
+++ spec/lib/yv_api_spec.rb
@@ -1,35 +1,29 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-require 'yv_api'
 require 'benchmark'
 
 describe YV::API::Client do
   describe ".get" do
-    it "gets a KJV booklist" do
-      list = YV::API::Client.get("bible/books", version: "kjv")
-      list[0].human.should == "Genesis"
+    it "gets from the API successfully" do
+      response = YV::API::Client.get("bible/configuration")
+      response.body["code"].should == 200
     end
-
 
     it "raises an informative exception if an API call fails" do
-      lambda do
-        YV::API::Client.get("bible/books", version: "kjvff")
-      end.should raise_error(RuntimeError, "API Error: Version is invalid")
+      response = YV::API::Client.get("bible/publisher")
+      response.body["data"]["errors"].should_not be_nil
     end
 
-    it "uses a block to recover from an API error if it exists" do
-      list = YV::API::Client.get("bible/books", version: "kjvff2") do | e |
-        e[0]["error"].should == "Version is invalid"
-        YV::API::Client.get("bible/books", version: "kjv")
-      end
-      list.first.human.should == "Genesis"
-    end
   end
 
   describe ".post" do
-    it "posts to the API" do
-      lambda do
-        YV::API::Client.post("users/authenticate", auth_username: "asdf", auth_password: "ghjkl")
-      end.should raise_error(RuntimeError, "API Error: Username or password is invalid")
+    it "posts to the API and receives an error" do
+      response = YV::API::Client.post("users/authenticate", auth: {username: "asdf", password: "ghjkl"}, debug: true)
+      response.body["data"]["errors"].should_not be_nil
+    end
+
+    it "posts to the API successfully" do
+      response = YV::API::Client.post("users/authenticate", auth: {username: "matt", password: "staging"}, debug: true)
+      response.should_not be_nil
     end
   end
 end
