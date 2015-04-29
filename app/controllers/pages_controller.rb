49ebfd1328f23d24ee12e73a5bdd970f9baf34c9
Michael Martin <michael.martin@lifechurch.tv>
2015-04-29 11:09:57 -0500

    Generic Error, Report to Sentry

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -48,22 +48,29 @@ class PagesController < ApplicationController
   end
 
   def donate
-    user = current_user
+    if params[:err].present?
+      Raven.capture do
+        raise TreadstoneAuthenticationError, "Treadstone Authentication Error: (Message: #{params[:err]}) "
+      end
+      render :generic_error
+    else
+      user = current_user
 
-    ts_payload = {
-      :created => Time.now.to_i.to_s,
-      :email => user.email,
-      :first_name => user.first_name,
-      :id => user.id.to_s,
-      :language_tag => user.language_tag,
-      :last_name => user.last_name,
-      :source => 'youversion'
-    }
+      ts_payload = {
+        :created => Time.now.to_i.to_s,
+        :email => user.email,
+        :first_name => user.first_name,
+        :id => user.id.to_s,
+        :language_tag => user.language_tag,
+        :last_name => user.last_name,
+        :source => 'youversion'
+      }
 
-    ts_signature = Licenses::Request.sign( ts_payload , ENV["TREADSTONE_SECRET"] ) unless ENV["TREADSTONE_SECRET"].nil?
-    ts_payload[:signature] = ts_signature
-    @ts_url = Cfg.treadstone_base_url + "?" + ts_payload.to_query
-    redirect_to @ts_url
+      ts_signature = Licenses::Request.sign( ts_payload , ENV["TREADSTONE_SECRET"] ) unless ENV["TREADSTONE_SECRET"].nil?
+      ts_payload[:signature] = ts_signature
+      @ts_url = Cfg.treadstone_base_url + "?" + ts_payload.to_query
+      redirect_to @ts_url
+    end
   end
 
 end
