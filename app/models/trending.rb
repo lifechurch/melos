0c67f95bba0052fc3a3b8fca65a59fc45196210d
Russ Calhoun <russ.calhoun@lifechurch.tv>
2015-05-27 15:29:43 -0500

    trending verses

---
+++ app/models/trending.rb
@@ -0,0 +1,61 @@
+# https://moments.youversionapistaging.com/3.1/trending.json
+
+class Trending < YV::Resource
+
+  include YV::Concerns::Moments
+
+  # attributes [:images, :moment_title, :expanded_dt, :category, :body_images]
+
+  class << self
+
+    # {
+    #     "response": {
+    #     "buildtime": "2015-03-04T18:34:33.327275+00:00",
+    #     "code": 200,
+    #     "data": [
+    #     "GEN.1.1",
+    #     "GEN.2.1",
+    #     "GEN.3.1",
+    #     "GEN.4.1",
+    #     "GEN.5.1",
+    #     "GEN.6.1",
+    #     "GEN.7.1",
+    #     "GEN.8.1",
+    #     "GEN.9.1",
+    #     "GEN.10.1",
+    #     "GEN.11.1",
+    #     "GEN.12.1",
+    #     "GEN.13.1",
+    #     "GEN.14.1",
+    #     "GEN.15.1",
+    #     "GEN.16.1",
+    #     "GEN.17.1",
+    #     "GEN.18.1",
+    #     "GEN.19.1",
+    #     "GEN.20.1",
+    #     "GEN.21.1",
+    #     "GEN.22.1",
+    #     "GEN.23.1",
+    #     "GEN.24.1",
+    #     "GEN.25.1",
+    #     "GEN.26.1",
+    #     "GEN.27.1",
+    #     "GEN.28.1",
+    #     "GEN.29.1",
+    #     "GEN.30.1"
+    # ]
+    # }
+    # }
+
+    def trending_path
+      "moments/trending"
+    end
+
+    def all()
+      data, errs = get(trending_path)
+      results = YV::API::Results.new(data,errs)
+      return results
+    end
+
+  end
+end
---
+++ lib/yv/api/mapper/trending.rb
@@ -0,0 +1,17 @@
+# module YV
+#   module API
+#     module Mapper
+#       class Trending < Base
+#
+#         class << self
+#
+#           def from_all(results)
+#             results
+#           end
+#
+#         end
+#
+#       end
+#     end
+#   end
+# end
