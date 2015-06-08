dd4030e7c74e9d8aaee334481a424f85415e2b1e
Will Platnick <wplatnick@gmail.com>
2015-06-08 16:57:52 -0400

    Remove db

--- db/migrate/20130904204124_create_kids_registration.rb
+++
@@ -1,8 +0,0 @@
-class CreateKidsRegistration < ActiveRecord::Migration
-  def change
-    create_table :kids_registrations do |t|
-      t.string :phone_number, limit: 15 
-      t.timestamps
-    end
-  end
-end
