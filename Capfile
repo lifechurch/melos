432a4af24778ec25d3f6e7b9604bd453b1ad60c0
Will Platnick <wplatnick@gmail.com>
2015-06-11 13:09:05 -0400

    Do asset compilation once locally

--- Capfile
+++ Capfile
@@ -1,21 +1,11 @@
 # Load DSL and set up stages
-require 'capistrano/setup'
-require 'capistrano/deploy'
+require "capistrano/setup"
+require "capistrano/deploy"
 require "capistrano/bundler"
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
 require "capistrano/rvm"
 require "capistrano/passenger"
-
-set :default_env, {
-  'FOG_DIRECTORY' => 'web-production',
-  'FOG_PROVIDER' => 'AWS',
-  'AWS_ACCESS_KEY_ID' => 'AKIAJTVJ5UNUPLF7N43Q',
-  'AWS_SECRET_ACCESS_KEY' => '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',
-  'SECURE_TRAFFIC' => true
-}
-
-set :passenger_restart_with_touch, true
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,26 @@
-role :web, %w{yvdep@web02-dfw.youversion.com}
+role :web, %w{yvdep@web01-dfw.youversion.com yvdep@web02-dfw.youversion.com}
+
+# Clear existing task so we can replace it rather than "add" to it.
+Rake::Task["deploy:compile_assets"].clear 
+ 
+namespace :deploy do
+  
+  desc 'Compile assets'
+  task :compile_assets => [:set_rails_env] do
+    # invoke 'deploy:assets:precompile'
+    invoke 'deploy:assets:precompile_local'
+    invoke 'deploy:assets:backup_manifest'
+  end
+   
+  namespace :assets do
+    
+    task :precompile_local do 
+      # compile assets locally
+      run_locally do
+        execute "source compilevars && RAILS_ENV=#{fetch(:stage)} ~/.rvm/bin/rvm ruby-1.9.3-p551@youversion-web do bundle exec rake assets:precompile"
+      end
+     end
+    
+  end
+ 
+end
