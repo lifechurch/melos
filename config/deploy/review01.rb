fbde89e38ca8d324e4a3149fabba357679bbfe88
Will Platnick <wplatnick@gmail.com>
2015-08-24 21:46:03 -0400

    Remove things that don't need to happen

--- config/deploy/review01.rb
+++ config/deploy/review01.rb
@@ -1,3 +1,7 @@
 role :web, %w{yvdep@webreview01-ny.youversion.com}
 set :bundle_flags, "--deployment"
 set :bundle_without, nil
+set :passenger_roles, :dummy
+
+Rake::Task["deploy:curl"].clear_actions
+
--- config/deploy/review02.rb
+++ config/deploy/review02.rb
@@ -1,3 +1,6 @@
 role :web, %w{yvdep@webreview02-ny.youversion.com}
 set :bundle_flags, "--deployment"
 set :bundle_without, nil
+set :passenger_roles, :dummy
+
+Rake::Task["deploy:curl"].clear_actions
