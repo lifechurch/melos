8f273f49ff6d7d387750d33ef3ffed4b850d28a1
Michael Martin <michael.martin@lifechurch.tv>
2015-08-27 14:59:08 -0500

    More Menu Stuff

--- app/assets/javascripts/header/header.js
+++ app/assets/javascripts/header/header.js
@@ -1,11 +1,19 @@
 angular.module('yv.header', [])
 
-.controller('HeaderCtrl', [function() {
-	console.log("Header! Woot!");
-  this.openMenu = function($mdOpenMenu, ev) {
+.controller('HeaderCtrl', ['$mdMenu', function($mdMenu) {
+	//this.isOpen = false;
+	//console.log("Header! Woot!");
+  this.openMenu = function($mdOpenMenu, ev, isOpen) {
   	ev.preventDefault();
     originatorEv = ev;
-    $mdOpenMenu(ev);
+		//console.log("MMM", isOpen, $mdOpenMenu, $mdMenu, $mdMenu.hide, $mdMenu.show);
+
+		//if (!this.isOpen) {
+			$mdOpenMenu(ev);
+			//this.isOpen = true;
+		//} else {
+			//this.isOpen = false;
+		//}
   };	
 }])
 
