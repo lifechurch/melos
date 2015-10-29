a8f20b73e29f0b55c2ea35ca7245c62fbe7c6d02
Michael Martin <michael.martin@lifechurch.tv>
2015-10-29 09:08:22 -0500

    $mdMenu should toggle open/close with trigger

--- app/assets/javascripts/header/header.js
+++ app/assets/javascripts/header/header.js
@@ -1,10 +1,22 @@
 angular.module('yv.header', [ 'header.notifications', 'header.friendships' ])
 
 .controller('HeaderCtrl', ['$mdMenu', function($mdMenu) {
-  this.openMenu = function($mdOpenMenu, ev) {
-  	ev.preventDefault();
-    originatorEv = ev;
-		$mdOpenMenu(ev);
+  var menuIsOpen = [];
+
+  function isOpen(key) {
+      return menuIsOpen.hasOwnProperty(key) && menuIsOpen[key];
+  }
+
+  this.openMenu = function($mdOpenMenu, ev, menuKey) {
+    ev.preventDefault();
+
+    if (isOpen(menuKey)) {
+        menuIsOpen[menuKey] = false;
+        $mdMenu.hide();
+    } else {
+        menuIsOpen[menuKey] = true;
+        $mdOpenMenu(ev);
+    }
   };	
 }])
 
