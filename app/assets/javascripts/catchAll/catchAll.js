12c9f38af85a2af2f852fbe04d6d09a56cad4e25
Michael Martin <michael.martin@lifechurch.tv>
2015-11-12 11:23:39 -0600

    Bug Fixes
    YV-6994, YV-6717, YV-6725, YV-6728, YV-6734, YV-6736: There were
    several issues when navigating the reader in non-English languages that
    should be fixed with this PR
    
    YV-6700: Offset issue with header when scrolling to top of page on AJAX
    reload should be fixed with this PR

--- app/assets/javascripts/catchAll/catchAll.js
+++ app/assets/javascripts/catchAll/catchAll.js
@@ -2,6 +2,82 @@ angular.module('yv.catchAll', [])
 
     .config([ '$stateProvider', function($stateProvider) {
         $stateProvider
+
+            .state('press', {
+                url: 		'/press',
+                controller: 'CatchAllCtrl'
+            })
+            .state('press-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/press',
+                controller: 'CatchAllCtrl'
+            })
+
+
+            .state('versions', {
+                url: 		'/versions',
+                controller: 'CatchAllCtrl'
+            })
+            .state('versions-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/versions',
+                controller: 'CatchAllCtrl'
+            })
+
+            .state('privacy', {
+                url: 		'/privacy',
+                controller: 'CatchAllCtrl'
+            })
+            .state('privacy-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/privacy',
+                controller: 'CatchAllCtrl'
+            })
+
+            .state('terms', {
+                url: 		'/terms',
+                controller: 'CatchAllCtrl'
+            })
+            .state('terms-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/terms',
+                controller: 'CatchAllCtrl'
+            })
+
+
+            .state('donate', {
+                url: 		'/donate',
+                controller: 'CatchAllCtrl'
+            })
+            .state('donate-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/donate',
+                controller: 'CatchAllCtrl'
+            })
+
+
+            .state('about', {
+                url: 		'/about',
+                controller: 'CatchAllCtrl'
+            })
+            .state('about-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/about',
+                controller: 'CatchAllCtrl'
+            })
+
+            .state('app', {
+                url: 		'/app',
+                controller: 'CatchAllCtrl'
+            })
+            .state('app-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/app',
+                controller: 'CatchAllCtrl'
+            })
+
+            .state('kids', {
+                url: 		'/kids',
+                controller: 'CatchAllCtrl'
+            })
+            .state('kids-locale', {
+                url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/kids',
+                controller: 'CatchAllCtrl'
+            })
+
             .state('videos', {
                 url: 		'/videos',
                 controller: 'CatchAllCtrl'
