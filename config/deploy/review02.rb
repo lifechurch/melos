5c09cdf4fe3a4ede9d04123340ed5037e503fcd9
Russ Calhoun <russ.calhoun@lifechurch.tv>
2015-11-18 14:24:10 -0600

    update configs for hip chat

--- config/deploy/review02.rb
+++ config/deploy/review02.rb
@@ -4,3 +4,10 @@ set :bundle_without, nil
 set :passenger_roles, :dummy
 
 Rake::Task["deploy:curl"].clear_actions
+
+set :hipchat_token, '7b1159a956abe4c9923b54fa3b5ef6'
+set :hipchat_room_name, 'YouVersion Web'
+set :hipchat_color, 'yellow'
+set :hipchat_success_color, 'green'
+set :hipchat_failed_color, 'red'
+set :hipchat_announce, true
