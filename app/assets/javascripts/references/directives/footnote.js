95ab421a00a9ae5ef03ee43504508c4e48035e55
Michael Martin <michael.martin@lifechurch.tv>
2015-11-19 09:23:02 -0600

    YV-6685
    Make it all work, yo.

--- app/assets/javascripts/references/directives/footnote.js
+++ app/assets/javascripts/references/directives/footnote.js
@@ -1,11 +1,27 @@
 angular.module('reader.footnote', [])
 
-.directive('note', function() {
+.factory('FootnoteCounter', function() {
+    var _notes = [];
+
+    return {
+        noteExists: function(key) {
+            return _notes.indexOf(key) > -1;
+        },
+
+        addNote: function(key) {
+            _notes.push(key);
+        }
+    };
+})
+
+.directive('note', [ 'FootnoteCounter', function(FootnoteCounter) {
 	return {
 		restrict: 'C',
 		compile: function(tElement, tAttrs, transclude) {
-			if (tElement[0] && tElement[0].children[1]) {
-				tElement[0].innerHTML = "<a href='#' tooltips tooltip-side='bottom' tooltip-try='1' tooltip-scroll='true' tooltip-title='tip' tooltip-html='" + tElement[0].children[1].innerHTML + "'>N</a>";
+            if (!FootnoteCounter.noteExists(tElement[0].textContent)) {
+                FootnoteCounter.addNote(tElement[0].textContent);
+            } else if (tElement[0] && tElement[0].children[1]) {
+				tElement[0].innerHTML = "<a href='#' tooltips tooltip-html='" + tElement[0].children[1].innerHTML + "'><img class='footnote-icon' src='/assets/footnote.png'></a>";
 			}
 		},
 		controller: ['$scope', '$element',  function($scope, $element) {
@@ -18,6 +34,6 @@ angular.module('reader.footnote', [])
 			});
 		}]
 	}
-})
+}])
 
 ;
