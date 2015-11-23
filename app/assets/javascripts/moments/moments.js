19fecab80fa21be106d54185fd4f899bc2273bf1
Michael Martin <michael.martin@lifechurch.tv>
2015-11-23 09:51:27 -0600

    Move Template to HAML and Localize 'Load More'

--- app/assets/javascripts/moments/moments.js
+++ app/assets/javascripts/moments/moments.js
@@ -21,28 +21,28 @@ angular.module('yv.moments', [
 	$stateProvider
 
 	.state('moments', {
-		url: 		'/moments',
-		controller: 	'MomentsCtrl',
-		template: 	'<div class="row moments-feed"><div class="medium-10 large-7 columns small-centered"><div ng-repeat="moment in moments"><moment data="moment" social-enabled="true"></moment></div><div layout="row" layout-sm="column" layout-align="space-around" ng-if="loading"><md-progress-circular md-mode="indeterminate"></md-progress-circular></div><button ng-click="loadMore()" class="solid-button green full ng-hide" ng-show="!loading && !pagingFinished">Load More</button></div></div>'
+		url: 		 '/moments',
+		controller:  'MomentsCtrl',
+        templateUrl: '/moment-feed.tpl.html'
 	})
 	.state('moments-locale', {
-		url: 		'/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/moments',
-		controller: 	'MomentsCtrl',
-		template: 	'<div class="row moments-feed"><div class="medium-10 large-7 columns small-centered"><div ng-repeat="moment in moments"><moment data="moment" social-enabled="true"></moment></div><div layout="row" layout-sm="column" layout-align="space-around" ng-if="loading"><md-progress-circular md-mode="indeterminate"></md-progress-circular></div><button ng-click="loadMore()" class="solid-button green full ng-hide" ng-show="!loading && !pagingFinished">Load More</button></div></div>'
+		url: 		 '/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/moments',
+		controller:  'MomentsCtrl',
+        templateUrl: '/moment-feed.tpl.html'
 	})
 
 
     .state('moment', {
-        url: '/moments/:momentId',
-        controller: 'MomentCtrl',
-        template: '<div class="row moments-feed"><div class="medium-10 large-7 columns small-centered"><moment data="moment" social-enabled="true"></moment></div></div>'
-    })
-    .state('moment-locale', {
-        url: '/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/moments/:momentId',
-        controller: 'MomentCtrl',
-        template: '<div class="row moments-feed"><div class="medium-10 large-7 columns small-centered"><moment data="moment" social-enabled="true"></moment></div></div>'
+        url:         '/moments/:momentId',
+        controller:  'MomentCtrl',
+        templateUrl: '/single-moment-feed.tpl.html'
     })
 
+    .state('moment-locale', {
+        url:         '/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/moments/:momentId',
+        controller:  'MomentCtrl',
+        templateUrl: '/single-moment-feed.tpl.html'
+    })
 
 	.state('profileActivity', {
 		url: 		'/users/:username',
