f2fdfb21037b726a3de8252bdcac66cff0e30cfc
Michael Martin <michael.martin@lifechurch.tv>
2015-12-14 13:34:44 -0600

    Angular Injection done Properly

--- app/assets/javascripts/common/fixTop.js
+++ app/assets/javascripts/common/fixTop.js
@@ -3,12 +3,12 @@ angular.module('common.fixTop', [])
 .directive('fixTop', function() {
 	return {
 		restrict: 'A',
-		controller: function($element, $window) {
+		controller: ['$element', '$window', function($element, $window) {
 			$element.css('position', 'relative');	
 			$window.onscroll = function() {
 				$element.css('top', $window.pageYOffset + "px");
 			};
-		}	
+		}]
 	};
 })
 
