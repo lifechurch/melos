82213ccc3af6f3f1186822ff5e246e0aa6985f53
Russ Calhoun <russ.calhoun@lifechurch.tv>
2016-01-21 14:49:08 -0600

    tweak this so the input is focused whenever it becomes visible

--- app/assets/javascripts/references/directives/reader.js
+++ app/assets/javascripts/references/directives/reader.js
@@ -58,4 +58,18 @@ angular.module('reader.reader',  [])
 	};
 }])
 
+.directive('focusMe', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function(value) {
+                if(value === true) {
+                    $timeout(function() {
+                        element[0].focus();
+                        scope[attrs.focusMe] = false;
+                    });
+                }
+            });
+        }
+    };
+});
 ;
