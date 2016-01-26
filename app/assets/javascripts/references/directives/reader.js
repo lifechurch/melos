c887fda9b5c6b2edb44cb84d305b16dec750156e
Russ Calhoun <russ.calhoun@lifechurch.tv>
2016-01-26 09:56:38 -0600

    move focusMe directive to common

--- app/assets/javascripts/references/directives/reader.js
+++ app/assets/javascripts/references/directives/reader.js
@@ -58,18 +58,4 @@ angular.module('reader.reader',  [])
 	};
 }])
 
-.directive('focusMe', function($timeout) {
-    return {
-        link: function(scope, element, attrs) {
-            scope.$watch(attrs.focusMe, function(value) {
-                if(value === true) {
-                    $timeout(function() {
-                        element[0].focus();
-                        scope[attrs.focusMe] = false;
-                    });
-                }
-            });
-        }
-    };
-});
 ;
