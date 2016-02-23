1cc1fdafde8d51dd63ce6e9bce3119df60b2bdad
Russ Calhoun <russ.calhoun@lifechurch.tv>
2016-02-22 18:05:37 -0600

    accept/display dropped image

--- app/features/EventEdit/features/content/components/ContentTypeImage.js
+++ app/features/EventEdit/features/content/components/ContentTypeImage.js
@@ -9,26 +9,30 @@ class ContentTypeImage extends Component {
     constructor(props) {
         super(props)
         this.state = { files: null };
-        // window.addEventListener("dragover",function(e){
-        //   e = e || event;
-        //   e.preventDefault();
-        // },false);
-        // window.addEventListener("drop",function(e){
-        //   e = e || event;
-        //   e.preventDefault();
-        // },false);
+        // prevent drag and drop from replacing the page when image is dropped outside of dropzone
+        window.addEventListener("dragover",function(e){
+          e = e || event;
+          e.preventDefault();
+        },false);
+        window.addEventListener("drop",function(e){
+          e = e || event;
+          e.preventDefault();
+        },false);
     }
 
     onDrop(files) {
-        console.log('upload file: ', files);
-        this.setState({
-            files: files
-        });
-    }
+        const { dispatch ,contentIndex, contentData } = this.props;
+        if (files[0].type === "image/jpeg" || files[0].type === "image/jpg") {
+            dispatch(ActionCreators.initUpload({index: contentIndex}));
 
-    initImageUpload() {
-        const { dispatch ,contentIndex } = this.props;
-        dispatch(ActionCreators.initUpload({index: contentIndex}));
+            this.setState({
+                files: files
+            })
+        } else {
+            // invalid file type
+        }
+        //console.log(this.props)
+        // console.log('upload file: ', files);
     }
 
     onOpenClick() {
@@ -37,49 +41,47 @@ class ContentTypeImage extends Component {
 
     render() {
         const { contentData, handleChange } = this.props
+        var output
+
+        if (this.state.files) {
+           output = <div>{this.state.files.map((file) => <img src={file.preview} />)}</div>
+        } else {
+            output = <div>
+                        <Dropzone ref='dropzone' onDrop={::this.onDrop} multiple={false} acceptedFiles=".pdf" className='image-drop-zone' activeClassName='active' >
+                            <div className='instructions'>
+                                <p>Drag and Drop an Image</p><br/>
+                                [JPG only]<br/>
+                                We recommend an image width of 1280px or more.<br/><br/>
+                                <a className='hollow-button green'>
+                                    Select Image
+                                </a>
+                            </div>
+                        </Dropzone>
+                    </div>
+        }
         return (
             <div className="form-body-block white">
-                <div className="columns medium-10 large-8 medium-offset-1 large-offset-2">
-                <button onClick={::this.initImageUpload}>imageAPICall</button>
-
+                    {output}
                     <div>
-                        <Dropzone ref='dropzone' onDrop={this.onDrop} multiple={false} className='image-drop-zone' activeClassName='active' >
-                            <div className='instructions'>Drag and Drop your Image<br/>JPG, PNG, GIF</div>
-                        </Dropzone>
-                        {this.state.files ? <div>
-                            <h2>Uploading file...</h2>
-                            <div>{this.state.files.map((file) => <img src={file.preview} />)}</div>
-                        </div> : null}
-                    </div>
-                </div>
-                <div>
-                image_id: {contentData.image_id}<br/>
-                body: {contentData.body}<br/>
-                url: {contentData.url}<br/>
-                </div>
-                <div className="columns medium-1 large-2">
-                    <a className='hollow-button green' onClick={::this.onOpenClick}>
-                        Select Image
-                    </a>
-                    <p className='image-drop-manual-reqs'>
-                        {this.props.instruction}
-                    </p>
-                </div>
-                <FormField
-                    InputType={Input}
-                    placeholder="body"
-                    name="body"
-                    onChange={handleChange}
-                    value={contentData.body}
-                    errors={contentData.errors} />
+                        <p>image_id: {contentData.image_id}</p>
+                        <p>body: {contentData.body}</p>
+                        <p>url: {contentData.url}</p>
+                        <FormField
+                            InputType={Input}
+                            placeholder="Add caption"
+                            name="body"
+                            onChange={handleChange}
+                            value={contentData.body}
+                            errors={contentData.errors} />
 
-                <FormField
-                    InputType={Input}
-                    placeholder="Image id"
-                    name="image_id"
-                    onChange={handleChange}
-                    value={contentData.image_id}
-                    errors={contentData.errors} />
+                        <FormField
+                            InputType={Input}
+                            placeholder="Image id"
+                            name="image_id"
+                            onChange={handleChange}
+                            value={contentData.image_id}
+                            errors={contentData.errors} />
+                    </div>
             </div>
         )
     }
