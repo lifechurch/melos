b41b60331c4c6ca6b23fbe2cb980b97d0ed59688
Russ Calhoun <russ.calhoun@lifechurch.tv>
2016-02-26 09:35:51 -0600

    fix retina, adjust mailto ampersands, remove console.logs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -348,6 +348,11 @@ module ApplicationHelper
     return doc.css('.content').text
   end
 
+  def image_tag_with_at2x(name_at_1x, options={})
+    name_at_2x = name_at_1x.gsub(%r{\.\w+$}, '@2x\0')
+    image_tag(name_at_1x, options.merge("data-at2x" => asset_path(name_at_2x)))
+  end
+
   private
   def lang_code(locale, host=nil)
     case host
