74b797110a815bb2fde545d7c2c4e6c2f4fe102d
Will Platnick <wplatnick@gmail.com>
2016-03-03 12:09:47 -0500

    More review stuff

--- config/deploy/review.rb
+++ config/deploy/review.rb
@@ -1,7 +1,7 @@
 require 'capistrano/datadog'
 set :datadog_api_key, "f0bc018e3f69961214ea44382eb0ad52"
 
-role :web, %w{yvdep@nodestaging01.web.dal01.thewardro.be}
+role :web, %w{yvdep@nodereview01.web.nyc03.thewardro.be}
 
 set :hipchat_token, '7b1159a956abe4c9923b54fa3b5ef6'
 set :hipchat_room_name, 'YouVersion Web'
