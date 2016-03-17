b31a3f5533bed9f0118e4ccd53799d2250fabd56
Will Platnick <wplatnick@gmail.com>
2016-03-16 22:01:34 -0400

    Update capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,4 @@
-require 'capistrano/datadog'
-set :datadog_api_key, "f0bc018e3f69961214ea44382eb0ad52"
-
-role :web, %w{yvdep@node01.web.dal01.thewardro.be yvdep@node02.web.dal01.thewardro.be}
+role :web, %w{yvdep@app01.node.dal01.thewardro.be yvdep@app02.web.dal01.thewardro.be}
 
 set :hipchat_token, '7b1159a956abe4c9923b54fa3b5ef6'
 set :hipchat_room_name, 'YouVersion Web'
