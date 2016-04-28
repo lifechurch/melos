695b122c1ad929fdaddcf11f73bef8d3d96d6b33
Michael Martin <michael.martin@life.church>
2016-04-28 11:27:28 -0500

    Fix a bug.

--- app/assets/javascripts/references/directives/verseAction.js
+++ app/assets/javascripts/references/directives/verseAction.js
@@ -13,7 +13,7 @@ angular.module('reader.verseAction', [])
 		templateUrl: '/reader-verse-action.tpl.html',
         controller: ['$scope', '$rootScope', function($scope, $rootScope) {
             $scope.$watch('isOpen', function(newVal, oldVal) {
-                if (newVal == false) {
+                if (newVal == false && !$scope.panelIsOpen) {
                     $rootScope.$emit('ClearVerseSelection');
                 }
             });
