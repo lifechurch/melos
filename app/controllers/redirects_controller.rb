84763d7948aeb6b97d7ff822537c357471edeb41
Russ Calhoun <russ.calhoun@lifechurch.tv>
2016-05-12 11:09:33 -0500

    add bafk afrikaans

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,6 +1,6 @@
 class RedirectsController < ApplicationController
 
-  before_filter :force_login, except: [:settings_notifications, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong]
+  before_filter :force_login, except: [:settings_notifications, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong, :kinderbybel]
 
   prepend_before_filter :mobile_redirect, only: [:bookmarks, :profile, :friends, :notes, :badges, :highlights, :connections]
   # skip_filter :set_page,
@@ -118,6 +118,11 @@ class RedirectsController < ApplicationController
   def kinderen
     # Dutch Redirect to Kids
     redirect_to('/nl/kids')
+  end
+
+  def kinderbybel
+    # Afrikaans Redirect to Kids
+    redirect_to('/af/kids')
   end
 
   def enfants
