ea46612826144290cbbb926c5aa1e1e7559094ed
Michael Martin <michael.martin@life.church>
2016-06-29 14:57:43 -0500

    Making some more corrections based on feedback from Design

--- app/assets/javascripts/plans/plans.js
+++ app/assets/javascripts/plans/plans.js
@@ -2,6 +2,25 @@ angular.module('yv.plans', [])
 
     .config([ '$stateProvider', function($stateProvider) {
         $stateProvider
+            .state('plans', {
+                url: '/reading-plans/:planSlug',
+                controller: 'PlansCtrl'
+            })
+            .state('plans-locale', {
+                url: '/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/reading-plans/:planSlug',
+                controller: 'PlansCtrl'
+            })
+
+            .state('plans-sample-day', {
+                url: '/reading-plans/:planSlug/day/:day',
+                controller: 'PlansCtrl'
+            })
+            .state('plans-sample-day-locale', {
+                url: '/{locale:[a-zA-Z]{2}(?:\-{1}[a-zA-Z]{2})*}/reading-plans/:planSlug/day/:day',
+                controller: 'PlansCtrl'
+            })
+
+
             .state('user-plan-edit', {
                 url: '/users/:user/reading-plans/:planSlug/edit',
                 controller: 'PlansCtrl'
@@ -25,8 +44,11 @@ angular.module('yv.plans', [])
 
     .controller('PlansCtrl', ['$scope', 'Subscription', '$window', function($scope, Subscription, $window) {
         $scope.showSubscribeButtons = false;
+        $scope.subscribing = false;
 
         $scope.startPlan = function(basePath, planId, privacy) {
+            $scope.subscribing = true;
+            $scope.showSubscribeButtons = false;
             Subscription.create(basePath, planId, privacy, null).success(function() {
                 $window.location.href = basePath + "/" + planId;
             }).error(function() {
