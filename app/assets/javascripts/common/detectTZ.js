0691cc532b43f53f46db3d2b8d98735280c24613
Jacob Allenwood <jacob.allenwood@life.church>
2016-07-11 12:42:42 -0500

    timezone directive

--- app/assets/javascripts/common/detectTZ.js
+++ app/assets/javascripts/common/detectTZ.js
@@ -4,17 +4,17 @@ angular.module('common.detectTZ', [])
         return {
             restrict: 'A',
             scope: {
-                userTZ: '='
+                userTz: '@'
             },
             controller: ["$element", "$scope", "$timeout", function($element, $scope, $timeout) {
 
                 $timeout(function() {
                     var tz = jstz.determine();
                     // if no user tz was given or they only have the default then set the determined tz to the element
-                    if(!$scope.userTZ || $scope.userTZ == 'Etc/UTC') {
+                    if(!$scope.userTz || $scope.userTz == 'Etc/UTC') {
                         $element.val(tz.name());
                     } else { // user already has updated tz and we want to set the element to that
-                        $element.val($scope.userTZ);
+                        $element.val($scope.userTz).change();
                     }
                 });
 
