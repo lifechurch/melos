f769046e43402e054bc47e84a6877e5fbd4e01de
Jacob Allenwood <jacob.allenwood@life.church>
2016-07-12 09:07:42 -0500

    handle different UTC cases

--- app/assets/javascripts/common/detectTZ.js
+++ app/assets/javascripts/common/detectTZ.js
@@ -11,7 +11,7 @@ angular.module('common.detectTZ', [])
                 $timeout(function() {
                     var tz = jstz.determine();
                     // if no user tz was given or they only have the default then set the determined tz to the element
-                    if(!$scope.userTz || $scope.userTz == 'Etc/UTC') {
+                    if( !$scope.userTz || ['etc/utc', 'utc'].indexOf($scope.userTz.toLowerCase()) != -1 ) {
                         $element.val(tz.name());
                     } else { // user already has updated tz and we want to set the element to that
                         $element.val($scope.userTz);
