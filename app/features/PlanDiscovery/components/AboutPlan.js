6a4030d74f69b884ae2ada9e56a959ebda5377af
Jacob Allenwood <jacob.allenwood@life.church>
2016-08-24 11:29:19 -0500

    component-ized about plan. share widget, subscribe user and save for later are all small components now

--- app/features/PlanDiscovery/components/AboutPlan.js
+++ app/features/PlanDiscovery/components/AboutPlan.js
@@ -3,9 +3,10 @@ import ActionCreators from '../actions/creators'
 import Carousel from '../../../components/Carousel/Carousel'
 import Image from '../../../components/Carousel/Image'
 import { Link } from 'react-router'
-import PlanActionButtons from '../components/PlanActionButtons'
+import PlanActionButtons from './PlanActionButtons'
 import { FormattedMessage } from 'react-intl'
 import AvatarList from '../../../components/AvatarList'
+import ShareWidget from './ShareWidget'
 
 class AboutPlan extends Component {
 
@@ -28,9 +29,7 @@ class AboutPlan extends Component {
 			)
 		}
 
-
 		var friendsReading, friendsCompleted, completions, readingList, completedList = null
-		var share = (this.state.dialogOpen) ? <a className='right video addthis_sharing_toolbox' onClick={::this.handleClick}>Share</a> : <a className='right share-panel' onClick={::this.handleClick}>Share</a>
 		var readingPlansStats = ((completions = readingPlan.stats.total_completed) >= 1000) ? <p className='friends_completed'><FormattedMessage id='plans.stats.total completions' values={{count: completions}} /></p> : null
 		var publisherUrl = (readingPlan.publisher_url) ? <a className='publisher'>{readingPlan.publisher_url}</a> : null
 
@@ -57,19 +56,19 @@ class AboutPlan extends Component {
 				<div className='columns large-8 medium-8'>
 					<div className='about-plan-header'>
 						<Link className='plans' to={`/en/reading-plans`}>&larr; Plans</Link>
-						{share}
+						<ShareWidget/>
 					</div>
 					<div className='plan-image'>
 						<Image width={720} height={400} thumbnail={false} imageId="false" type="about_plan" config={readingPlan} />
 					</div>
 					<div className='row'>
 						<div className='columns large-8 medium-8'>
-							<h1>{readingPlan.name.default}</h1>
-							<p className='plan_length'>{readingPlan.total_days}</p>
-							<p className='plan_about'>{readingPlan.about.text.default}</p>
+							<h1>{ readingPlan.name.default }</h1>
+							<p className='plan_length'><FormattedMessage id='plans.total_days' values={{ count: readingPlan.total_days }}/></p>
+							<p className='plan_about'>{ readingPlan.about.text.default }</p>
 							<h3 className='publisher'><FormattedMessage id='plans.publisher'/></h3>
-							<p className='publisher'>{readingPlan.copyright.text.default}</p>
-							{publisherUrl}
+							<p className='publisher'>{ readingPlan.copyright.text.default }</p>
+							<Link className='plans' to={publisherUrl}><FormattedMessage id='plans.about publisher'/></Link>
 						</div>
 						<div className='columns large-4 medium-4'>
 							<div className='side-col'>
@@ -77,9 +76,9 @@ class AboutPlan extends Component {
 							</div>
 							<hr></hr>
 							<div className='stats'>
-								{friendsReading}
-								{friendsCompleted}
-								{readingPlansStats}
+								{ friendsReading }
+								{ friendsCompleted }
+								{ readingPlansStats }
 							</div>
 						</div>
 					</div>
---
+++ app/features/PlanDiscovery/components/ShareWidget.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react'
+import ActionCreators from '../actions/creators'
+import Carousel from '../../../components/Carousel/Carousel'
+import Image from '../../../components/Carousel/Image'
+import { Link } from 'react-router'
+import PlanActionButtons from '../components/PlanActionButtons'
+import { FormattedMessage } from 'react-intl'
+import AvatarList from '../../../components/AvatarList'
+
+class ShareWidget extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = { dialogOpen: false }
+	}
+
+	handleClick() {
+		this.setState({ dialogOpen: !this.state.dialogOpen })
+	}
+
+
+	render() {
+		const collapsible = this.props.collapsible || true
+
+		if (collapsible) {
+			if (this.state.dialogOpen) {
+				var share = <a className='video addthis_sharing_toolbox' onClick={::this.handleClick}><FormattedMessage id='share'/></a>
+			} else {
+				var share = <a className='share-panel' onClick={::this.handleClick}><FormattedMessage id='share'/></a>
+			}
+		} else {
+			var share = <a className='video addthis_sharing_toolbox'><FormattedMessage id='share'/></a>
+		}
+
+		return (
+			<p className='share'>{ share }</p>
+		)
+	}
+}
+
+export default ShareWidget
