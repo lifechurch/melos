ef4b7514f01c520b2ab935d73aee6afd8b0f3b04
Michael Martin <michael.martin@life.church>
2016-08-26 14:13:51 -0500

    Wire up links for My Plans & Discover top-level buttons

--- app/containers/PlansView.js
+++ app/containers/PlansView.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { FormattedMessage } from 'react-intl'
+import { Link } from 'react-router'
 
 class PlansView extends Component {
 	render() {
-		const { children } = this.props
+		const { children, auth } = this.props
+
+		const myPlansLink = auth.isLoggedIn && auth.userData && auth.userData.username ?
+			(<a className='solid-button green' href={`/users/${auth.userData.username}/reading-plans`}><FormattedMessage id="plans.my_plans" /></a>) :
+			(<a className='solid-button green' href='/sign-in'><FormattedMessage id="plans.my_plans" /></a>)
 
 		return (
 			<div>
 				<div className='row horizontal-center discover-buttons'>
 					<ul className='button-group primary-toggle'>
-						<li><a className='solid-button green' href='#'>Discover</a></li>
-						<li className='inactive'><a className='solid-button green' href='#'>My Plans</a></li>
+						<li><Link to='/reading-plans' className='solid-button green'><FormattedMessage id="plans.discover" /></Link></li>
+						<li className='inactive'>{ myPlansLink }</li>
 					</ul>
 				</div>
 				{children}
@@ -18,4 +25,10 @@ class PlansView extends Component {
 	}
 }
 
-export default PlansView
+function mapStateToProps(state) {
+	return {
+		auth: state.auth
+	}
+}
+
+export default connect(mapStateToProps, null)(PlansView)
