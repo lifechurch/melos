8f0a813c0246e2f3d3a2c8e8a6efbeb28a1d9c4f
Jacob Allenwood <jacob.allenwood@life.church>
2016-09-12 15:39:17 -0500

    adding slidesToShow will prevent the carousel from getting stuck on the last slide (slick is also working on getting rid of the blank space on the end of the carousel)

--- app/components/Carousel/CarouselStandard.js
+++ app/components/Carousel/CarouselStandard.js
@@ -8,29 +8,30 @@ import { FormattedMessage } from 'react-intl'
 import { Link } from 'react-router'
 
 class CarouselStandard extends Component {
+
   render() {
 		const { carouselContent, imageConfig, localizedLink, context, isRtl } = this.props
+
 		var carouselTitle = (carouselContent.title) ? carouselContent.title : <FormattedMessage id='plans.related plans' />
 		// for a dynamic collection we need to build, the header link will be different
 		var carouselLink = (context == 'recommended') ? `/recommended-plans-collection/${carouselContent.id}` : (context == 'saved') ? `/saved-plans-collection` : `/reading-plans-collection/${carouselContent.id}`
 
-
+		const slideStyle = isRtl() ? { display: 'inline-block' } : {}
 
     var settings = {
-    	centerMode: false,
+			centerMode: false,
       infinite: false,
       variableWidth: true,
+      slidesToScroll: 2,
+      slidesToShow: 2,
       arrows: true,
-      slidesToScroll: 3,
       rtl: isRtl(),
       prevArrow: <CarouselArrow dir='left' fill='gray' width={19} height={19}/>,
       nextArrow: <CarouselArrow dir='right' fill='gray' width={19} height={19}/>,
       responsive: [ {
-      	breakpoint: 524, settings: { arrows: false }
+      	breakpoint: 524, settings: { arrows: false, slidesToShow: 1 }
       } ]
-    };
-
-		const slideStyle = isRtl() ? { display: 'inline-block' } : {}
+		}
 
     // we want an image first, if that doesn't exist then we go to gradient, if gradient doesn't exist then just set default plan image
     var slides = carouselContent.items.map( function(slide, index) {
