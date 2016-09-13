eb288a1937f053a86eee19ef5d977f932dc77c55
Russ Calhoun <russ.calhoun@life.church>
2016-09-13 15:39:59 -0500

    add italian bafk and update thai logo

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,6 +1,6 @@
 class RedirectsController < ApplicationController
 
-  before_filter :force_login, except: [:settings_notifications, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong, :kinderbybel, :pambatang, :thaibafk, :thieunhi]
+  before_filter :force_login, except: [:settings_notifications, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong, :kinderbybel, :pambatang, :thaibafk, :thieunhi, :ragazzi]
 
   prepend_before_filter :mobile_redirect, only: [:bookmarks, :profile, :friends, :notes, :badges, :highlights, :connections]
   # skip_filter :set_page,
@@ -159,5 +159,9 @@ class RedirectsController < ApplicationController
 
   def thieunhi
     redirect_to('/vi/kids')
+  end
+
+  def ragazzi
+    redirect_to('/it/kids')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ YouversionWeb::Application.routes.draw do
   get "/%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%81", to: "redirects#thaibafk"
   get "/thieunhi",       to: "redirects#thieunhi"
   get "/thi%E1%BA%BFunhi", to: "redirects#thieunhi"
+  get "/ragazzi",        to: "redirects#ragazzi"
 
   get "/trending-bible-verses", to: "pages#trending"
   get "/verse-of-the-day", to: "pages#votd"
