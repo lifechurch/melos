f421cfdcec8794edb18fe39eaa174f2203707531
Simon Roberts <simon.roberts@life.church>
2016-09-22 14:36:25 -0500

    remove datadog references

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,3 @@
-require 'capistrano/datadog'
-set :datadog_api_key, "f0bc018e3f69961214ea44382eb0ad52"
-
 role :web, %w{yvdep@ruby01.web.dal01.thewardro.be yvdep@ruby02.web.dal01.thewardro.be}
 
 set :hipchat_token, '7b1159a956abe4c9923b54fa3b5ef6'
