f59a240e07412fb2a13cf9dd61372c1a2cc471ca
Jacob Allenwood <jacob.allenwood@life.church>
2016-10-28 17:48:21 -0500

    label list is working. need to build group headings for alphabetic filter

--- app/features/Bible/components/verseAction/bookmark/LabelList.js
+++ app/features/Bible/components/verseAction/bookmark/LabelList.js
@@ -1,8 +1,31 @@
 import React, { Component, PropTypes } from 'react'
+import LabelPill from './LabelPill'
 
 class LabelList extends Component {
+
 	render() {
-		return (<div>LabelList</div>)
+		const { list, onSelect, filteredBy } = this.props
+
+		let labels = null
+
+		if (list) {
+			labels = list.map((label, index) => {
+				return (
+					<LabelPill
+						label={label.label}
+						count={label.count}
+						canDelete={false}
+						onSelect={onSelect}
+					/>
+				)
+			})
+		}
+
+		return (
+			<div className='label-list'>
+				{ labels }
+			</div>
+		)
 	}
 }
 
