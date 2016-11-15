845fc469c9827ac1539bae9884734aca1aaa6263
Michael Martin <michael.martin@life.church>
2016-11-15 14:27:21 -0600

    no message

--- app/features/Bible/components/audio/AudioPlayer.js
+++ app/features/Bible/components/audio/AudioPlayer.js
@@ -205,11 +205,11 @@ class AudioPlayer extends Component {
 	}
 
 	handleYieldToStandalone() {
-		const { audio } = this.props
+		const { audio, hosts } = this.props
 		LocalStore.set("standaloneAudioPlayerState", Object.assign({}, this.state, { standaloneWindow: null }))
 		LocalStore.set("standaloneAudioPlayerAudio", audio)
 		this.handlePlayerPauseRequest()
-		let standaloneWindow = window.open('http://localhost:3000/en-US/bible-audio-player', '_blank')
+		let standaloneWindow = window.open('${hosts.nodeHost}/en-US/bible-audio-player', '_blank')
 		this.setState({ hasStandalone: true, standaloneWindow: standaloneWindow })
 	}
 
