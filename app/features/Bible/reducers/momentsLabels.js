7b5226c6a2181fb1cddf5d40e5f65f9bbe58c1bf
Jacob Allenwood <jacob.allenwood@life.church>
2016-12-02 13:31:12 -0600

    rewrite label heading algorithm because different JavaScript interpreters treat .sort() differently, and we were getting weird results using sort for the heading algorithm.

--- app/features/Bible/reducers/momentsLabels.js
+++ app/features/Bible/reducers/momentsLabels.js
@@ -18,26 +18,33 @@ export default function reducer(state = {}, action) {
 				// return 0 if values are equal
 				// localeCompare does lowercase and unicode compare
 				// we can also pass in language_tag for locale
-				let val = a.label.toLowerCase().localeCompare(b.label.toLowerCase(), { sensitivity: 'base' })
-
-				return val
+				return a.label.toLowerCase().localeCompare(b.label.toLowerCase(), { sensitivity: 'base' })
 			})
+
 			// if the first sorted label is not an alphabetical character set up the swagtag header
+			// we need to do this so we don't keep setting the # multiple times for different labels
+			// in the following loop
 			if (!byAlphabetical[0].label.charAt(0).match(/^[a-zA-Z]/)) {
 				byAlphabetical[0].groupHeading = '#'
 			} else {
 				byAlphabetical[0].groupHeading = byAlphabetical[0].label.charAt(0).toUpperCase()
 			}
-			byAlphabetical.sort((a, b) => {
-				// build headers for alphabetic groupings
-				// compare the first character of each label
-				// if the label starts with a letter set up the header once per letter appearance
-				if (a.label.charAt(0).match(/^[a-zA-Z]/) && (a.label.charAt(0).toLowerCase().localeCompare(b.label.charAt(0).toLowerCase(), { sensitivity: 'base' })) !== 0) {
-					a.groupHeading = a.label.charAt(0).toUpperCase()
-				} else {
-					a.groupHeading = null
+
+			// build headers for alphabetic groupings
+			// compare the first character of each label
+			// if the label starts with a letter set up the header once per letter appearance
+			for (let index = 2; index < byAlphabetical.length; index++) {
+				if (index < byAlphabetical.length) {
+					let a = byAlphabetical[index - 1]
+					let b = byAlphabetical[index]
+
+					if (a.label.charAt(0).match(/^[a-zA-Z]/) && (a.label.charAt(0).toLowerCase().localeCompare(b.label.charAt(0).toLowerCase(), { sensitivity: 'base' })) !== 0) {
+						b.groupHeading = b.label.charAt(0).toUpperCase()
+					} else {
+						b.groupHeading = null
+					}
 				}
-			})
+			}
 
 			return Immutable.fromJS({ byCount, byAlphabetical }).toJS()
 
