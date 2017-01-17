2e4e7ee378e9e1697fca39491ee1b8a1a7a6c796
Michael Martin <michael.martin@life.church>
2017-01-17 12:14:40 -0600

    Making new Routes for RP

--- app/standalone/PlanDiscovery/routes.js
+++ app/standalone/PlanDiscovery/routes.js
@@ -21,6 +21,32 @@ export default function(requirePlanDiscoveryData, requirePlanCollectionData, req
 			<Route path="(:lang/)recommended-plans-collection" component={PlansView}>
 				<Route path=":id(-:slug)" component={PlanCollectionView} onEnter={requireRecommendedPlanData} />
 			</Route>
+			<Route path="(:lang/)users/:username">
+				<Route path="saved-reading-plans" component={MySavedPlansView} />
+				<Route path="completed-reading-plans" component={MyCompletedPlansView} />
+				<Route path="reading-plans">
+					<IndexRoute component={MyPlansView} />
+					<Route path=":id(-:slug)">
+						<IndexRoute component={MyPlanDayView} />
+						<Route path="devo" component={MyPlanDayDevoView} />
+						<Route path="ref" component={MyPlanDayRefView} />
+						<Route path="edit" component={MyPlanSettingsView} />
+						<Route path="calendar" component={MyPlanCalendarView} />
+					</Route>
+				</Route>
+			</Route>
 		</Route>
 	)
 }
+
+/*
+MyPlansView
+MySavedPlansView
+MyCompletedPlansView
+
+MyPlanDayView
+MyPlanDayDevoView
+MyPlanDayRefView
+MyPlanSettingsView
+MyPlanCalendarView
+*/
