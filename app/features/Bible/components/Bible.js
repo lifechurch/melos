9324d83c257e9d1b62d5a53bcaf63700c8395f21
Michael Martin <michael.martin@life.church>
2017-03-31 15:27:41 -0500

    Remove `Change Version` button from Bible Plans.

--- app/features/Bible/components/Bible.js
+++ app/features/Bible/components/Bible.js
@@ -71,13 +71,6 @@ class Bible extends Component {
 		const { dispatch, bible, auth } = this.props
 		const { chapterError } = this.state
 
-		// check for cookie written from reading plans telling
-		// bible to open with the chapterpicker modal open
-		if (LocalStore.get('showPickerOnLoad') || chapterError || bible.chapter.showError) {
-			this.chapterPickerInstance.openDropdown()
-			LocalStore.delete('showPickerOnLoad')
-		}
-
 		this.recentVersions = new RecentVersions()
 		this.recentVersions.syncVersions(bible.settings)
 		this.updateRecentVersions()
