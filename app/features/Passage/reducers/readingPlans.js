433483765d32b4c69db3259b70daf1113d71281e
Jacob Allenwood <jacob.allenwood@life.church>
2017-05-02 11:31:40 -0500

    - reducer data check for data key change

--- app/features/Passage/reducers/readingPlans.js
+++ app/features/Passage/reducers/readingPlans.js
@@ -13,19 +13,22 @@ export default function reducer(state = {}, action) {
 
 		case type('readingplansPlansByReferenceSuccess'):
 			return (function readingplansPlansByReferenceSuccess() {
-				let plans = action.response.reading_plans.map((plan) => {
-					return Immutable.fromJS(plan).set('image_id', plan.id).set('type', 'reading_plan').toJS()
-				})
-				// for quicker load times, let's just show 6 plans
-				plans = plans.slice(0, 6)
-				return Immutable
+				if (action.response && 'reading_plans' in action.response) {
+					let plans = action.response.reading_plans.map((plan) => {
+						return Immutable.fromJS(plan).set('image_id', plan.id).set('type', 'reading_plan').toJS()
+					})
+					// for quicker load times, let's just show 6 plans
+					plans = plans.slice(0, 6)
+					return Immutable
 					.fromJS(action.response)
 					.merge({ items: plans, type: false })
 					.delete('loading')
 					.delete('plans')
 					.toJS()
+				} else {
+					return state
+				}
 			}())
-
 		default:
 			return state
 	}
