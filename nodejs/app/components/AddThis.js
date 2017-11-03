0b2cbc852861f643c45af93e3c625a27e261abe1
Jacob Allenwood <jacob.allenwood@life.church>
2017-11-03 16:46:37 -0500

    - revert addthis

--- nodejs/app/components/AddThis.js
+++ nodejs/app/components/AddThis.js
@@ -3,32 +3,6 @@ import React, { Component, PropTypes } from 'react'
 
 class AddThis extends Component {
 	componentDidMount() {
-		this.initialize()
-	}
-
-	componentWillReceiveProps(nextProps) {
-		const { title: nextTitle, text: nextText, url: nextUrl } = nextProps
-		const { title, text, url } = this.props
-
-		if (
-			typeof window !== 'undefined' &&
-			(text !== nextText || url !== nextUrl || title !== nextTitle)
-		) {
-			if (typeof window.addthis_share !== 'object') {
-				window.addthis_share = {}
-			}
-			if (window.addthis) {
-				window.addthis.update('share', 'url', nextUrl)
-				window.addthis.update('share', 'title', `${nextTitle || nextText}`)
-				window.addthis.update('share', 'description', nextText)
-			} else {
-				this.initialize()
-			}
-		}
-	}
-
-	initialize = () => {
-		const { url, text, title } = this.props
 		if (typeof window !== 'undefined') {
 			// Initialize AddThis, if Necessary
 			const interval = setInterval(() => {
@@ -39,16 +13,36 @@ class AddThis extends Component {
 				) {
 					clearInterval(interval);
 					window.addthis.layers.refresh()
-					window.addthis.update('share', 'url', url)
-					window.addthis.update('share', 'title', `${title || text}`)
-					window.addthis.update('share', 'description', text)
 				}
 			}, 100);
 		}
 	}
 
+	shouldComponentUpdate(nextProps) {
+		const { title: nextTitle, text: nextText, url: nextUrl } = nextProps
+		const { title, text, url } = this.props
+
+		if (
+			typeof window !== 'undefined' &&
+			(text !== nextText || url !== nextUrl || title !== nextTitle)
+		) {
+			if (typeof window.addthis_share !== 'object') {
+				window.addthis_share = {}
+			}
+
+			window.addthis.update('share', 'url', nextUrl)
+			window.addthis.update('share', 'title', `${nextTitle || nextText}`)
+			window.addthis.update('share', 'description', nextText)
+
+			return true
+		}
+
+		return false
+	}
+
 	render() {
 		const { className } = this.props
+
 		return (
 			<div className={className}>
 				<div className='addthis_inline_share_toolbox_a0vl' />
@@ -59,14 +53,13 @@ class AddThis extends Component {
 
 AddThis.propTypes = {
 	title: PropTypes.string,
-	text: PropTypes.string,
+	text: PropTypes.string.isRequired,
 	url: PropTypes.string.isRequired,
 	className: PropTypes.string,
 }
 
 AddThis.defaultProps = {
-	title: null,
-	text: null,
+	title: '',
 	className: '',
 }
 
