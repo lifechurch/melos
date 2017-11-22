bd757fe17acb76a4fc03c10b35aad35f7dac4e5b
Britt Mileshosky <mileshosky@gmail.com>
2017-11-22 15:03:45 -0600

    generate proper url for node host.

--- nodejs/app/standalone/YearInReview/mapParamsToState.js
+++ nodejs/app/standalone/YearInReview/mapParamsToState.js
@@ -1,7 +1,7 @@
 export default function mapParamsToState(state, params) {
-  return Object.assign({}, state, {
-    userId: params.user_id,
-    serverLanguageTag: params.languageTag,
-    nodeHost: process.env.SECURE_HOSTNAME || 'http://localhost:3000'
-  })
+	return Object.assign({}, state, {
+		userId: params.user_id,
+		serverLanguageTag: params.languageTag,
+		nodeHost: `//${process.env.SECURE_HOSTNAME}` || 'http://localhost:3000'
+	})
 }
