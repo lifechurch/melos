e3f2c6f525c65628c3b8383349a211c3f60d18ea
Jacob Allenwood <jacob.allenwood@life.church>
2017-12-15 19:36:29 -0600

    - wip add versions list to passage

--- nodejs/app/features/Passage/components/Passage.js
+++ nodejs/app/features/Passage/components/Passage.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { connect } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
 import Helmet from 'react-helmet'
@@ -9,7 +9,12 @@ import withImagesData from '@youversion/api-redux/lib/endpoints/images/hocs/with
 import withReferenceData from '@youversion/api-redux/lib/endpoints/bible/hocs/withReference'
 import selectImageFromList from '@youversion/utils/lib/images/selectImageFromList'
 import Routes from '@youversion/utils/lib/routes/routes'
+import localeVersions from '@youversion/stringer-things/dist/config/localeVersions.json'
+import Card from '@youversion/melos/dist/components/containers/Card'
+import Link from '@youversion/melos/dist/components/links/Link'
+import VerticalSpace from '@youversion/melos/dist/components/layouts/VerticalSpace'
 import DropDownArrow from '../../../components/DropDownArrow'
+import FullscreenDrawer from '../../../components/FullscreenDrawer'
 import { buildMeta } from '../../../lib/readerUtils'
 import ReferenceMoment from '../../Moments/components/types/ReferenceMoment'
 import ImagesMoment from '../../Moments/components/types/VotdImage'
@@ -17,88 +22,133 @@ import ShareSheet from '../../../widgets/ShareSheet/ShareSheet'
 import PlansRelatedToReference from '../../../widgets/PlansRelatedToReference'
 
 
-function Passage(props) {
-	const {
-		usfm,
-		titleWithAbbr,
-		versionAbbr,
-		versionId,
-		text,
-		hosts,
-		serverLanguageTag,
-		bible,
-		images,
-	} = props
+class Passage extends Component {
 
-	const version = bible && bible.pullVersion(versionId)
-	const title = `${titleWithAbbr}; ${text}`
-	const url = `${hosts.railsHost}${Routes.reference({
-		usfm,
-		version_id: versionId,
-		version_abbr: versionAbbr,
-		serverLanguageTag,
-	})}`
-	const metaLink = (version && buildMeta({ hosts, version, usfm }).link) || []
-	// const nextLink = localizedLink(`/bible/${versionId}/${primaryVersion.nextVerse}.${version.local_abbreviation.toLowerCase()}`)
-	// const prevLink = localizedLink(`/bible/${versionId}/${primaryVersion.previousVerse}.${version.local_abbreviation.toLowerCase()}`)
-	let imgMeta = []
-	if (images && images.length > 0) {
-		const img = images[0]
-		const src = selectImageFromList({
-			images: img.renditions,
-			width: 640,
-			height: 640,
-		}).url
-		imgMeta = [
-			{ property: 'og:image', content: `https:${src}` },
-			{ property: 'og:image:width', content: 640 },
-			{ property: 'og:image:height', content: 640 },
-			{ name: 'twitter:image', content: `https:${src}` }
-		]
+	constructor(props) {
+		super(props)
+		this.state = {
+			versionsOpen: false,
+		}
 	}
 
-	const versionDropdown = (
-		<a
-			tabIndex={0}
-			className="dropdown-button vertical-center"
-			target="_self"
-			onClick={null}
-		>
-			<div className='flex-wrap' style={{ flex: 4, lineHeight: '1.5' }}>
-				<div style={{ fontSize: '11px', width: '100%' }}><FormattedMessage id='version' /></div>
-				<div style={{ fontSize: '14px', color: 'black' }}>{ versionAbbr }</div>
-			</div>
-			<div className='vertical-center' style={{ flex: 1 }}>
-				<DropDownArrow fill="#DDDDDD" height={10} dir='down' />
-			</div>
-		</a>
-	)
+	handleOpenVersions = () => {
+		this.setState({ versionsOpen: true })
+	}
 
-	return (
-		<div className='gray-background horizontal-center' style={{ padding: '50px 0' }}>
-			<Helmet
-				title={title}
-				meta={[
-					{ name: 'description', content: text },
-					{ property: 'og:title', content: title },
-					{ property: 'og:url', content: url },
-					{ property: 'og:description', content: text },
-					{ name: 'twitter:card', content: 'summary' },
-					{ name: 'twitter:url', content: url },
-					{ name: 'twitter:title', content: title },
-					{ name: 'twitter:description', content: text },
-					{ name: 'twitter:site', content: '@YouVersion' },
-				].concat(imgMeta)}
-				link={metaLink}
-			/>
-			<div className='yv-large-5 yv-medium-7 yv-small-11 votd-view'>
-				<ReferenceMoment usfm={usfm} version_id={versionId} leftFooter={[versionDropdown]} />
-				<ImagesMoment usfm={usfm} version_id={versionId} />
-				<PlansRelatedToReference usfm={usfm} version_id={versionId} />
+	render() {
+		const {
+			usfm,
+			titleWithAbbr,
+			versionAbbr,
+			versionId,
+			text,
+			hosts,
+			serverLanguageTag,
+			bible,
+			images,
+		} = this.props
+		const { versionsOpen } = this.state
+
+		const version = bible && bible.pullVersion(versionId)
+		const title = `${titleWithAbbr}; ${text}`
+		const url = `${hosts.railsHost}${Routes.reference({
+			usfm,
+			version_id: versionId,
+			version_abbr: versionAbbr,
+			serverLanguageTag,
+		})}`
+		const metaLink = (version && buildMeta({ hosts, version, usfm }).link) || []
+		// const nextLink = localizedLink(`/bible/${versionId}/${primaryVersion.nextVerse}.${version.local_abbreviation.toLowerCase()}`)
+		// const prevLink = localizedLink(`/bible/${versionId}/${primaryVersion.previousVerse}.${version.local_abbreviation.toLowerCase()}`)
+		let imgMeta = []
+		if (images && images.length > 0) {
+			const img = images[0]
+			const src = selectImageFromList({
+				images: img.renditions,
+				width: 640,
+				height: 640,
+			}).url
+			imgMeta = [
+				{ property: 'og:image', content: `https:${src}` },
+				{ property: 'og:image:width', content: 640 },
+				{ property: 'og:image:height', content: 640 },
+				{ name: 'twitter:image', content: `https:${src}` }
+			]
+		}
+
+		const versionDropdown = (
+			<a
+				tabIndex={0}
+				className="dropdown-button vertical-center"
+				target="_self"
+				onClick={this.handleOpenVersions}
+			>
+				<div className='flex-wrap' style={{ flex: 4, lineHeight: '1.5' }}>
+					<div style={{ fontSize: '11px', width: '100%' }}><FormattedMessage id='version' /></div>
+					<div style={{ fontSize: '14px', color: 'black' }}>{ versionAbbr }</div>
+				</div>
+				<div className='vertical-center' style={{ flex: 1 }}>
+					<DropDownArrow fill="#DDDDDD" height={10} dir='down' />
+				</div>
+			</a>
+		)
+
+		const versions = localeVersions.en && localeVersions.en.text
+
+		return (
+			<div className='gray-background horizontal-center' style={{ padding: '50px 0' }}>
+				<Helmet
+					title={title}
+					meta={[
+						{ name: 'description', content: text },
+						{ property: 'og:title', content: title },
+						{ property: 'og:url', content: url },
+						{ property: 'og:description', content: text },
+						{ name: 'twitter:card', content: 'summary' },
+						{ name: 'twitter:url', content: url },
+						{ name: 'twitter:title', content: title },
+						{ name: 'twitter:description', content: text },
+						{ name: 'twitter:site', content: '@YouVersion' },
+					].concat(imgMeta)}
+					link={metaLink}
+				/>
+				<div className='yv-large-5 yv-medium-7 yv-small-11 votd-view'>
+					<ReferenceMoment usfm={usfm} version_id={versionId} leftFooter={[versionDropdown]} />
+					<ImagesMoment usfm={usfm} version_id={versionId} />
+					<PlansRelatedToReference usfm={usfm} version_id={versionId} />
+				</div>
+				<FullscreenDrawer
+					isOpen={versionsOpen}
+					onClose={() => { this.setState({ versionsOpen: false }) }}
+					className='yv-large-3 yv-small-11 centered'
+					title={<FormattedMessage id='select version' />}
+				>
+					<VerticalSpace>
+						{
+							versions && versions.map((v) => {
+								return (
+									<Link
+										key={v}
+										to={Routes.reference({
+											version_id: v,
+											usfm,
+											version_abbr: versionAbbr,
+											serverLanguageTag
+										})}
+									>
+										<Card customClass='horizontal-center flex-wrap'>
+											{ v }
+										</Card>
+									</Link>
+								)
+							})
+							}
+					</VerticalSpace>
+				</FullscreenDrawer>
+				<ShareSheet />
 			</div>
-			<ShareSheet />
-		</div>
-	)
+		)
+	}
 }
 
 Passage.propTypes = {
