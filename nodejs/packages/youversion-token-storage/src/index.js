7e043b2aa2bc2ecebeb0334127c5191cb3b8f832
Michael Martin <martinmsm@gmail.com>
2018-03-15 14:01:37 -0500

    Change domain scope of YouVersionToken cookie to .bible.com or localhost

--- nodejs/packages/youversion-token-storage/src/index.js
+++ nodejs/packages/youversion-token-storage/src/index.js
@@ -3,12 +3,16 @@ import cookie from 'react-cookie'
 const TOKEN_STORAGE_KEY = 'YouVersionToken'
 const TOKEN_MAX_AGE = 60 * 60 * 24
 
-export function fetchToken () {
+export function fetchToken() {
 	return cookie.load(TOKEN_STORAGE_KEY)
 }
 
 export function storeToken(token, maxAge = TOKEN_MAX_AGE) {
-	cookie.save(TOKEN_STORAGE_KEY, token, { path: '/', maxAge })
+	const windowHasHostname = (window && window.location && window.location.hostname && window.location.hostname !== 'localhost')
+	const domain = windowHasHostname
+    ? [''].concat(window.location.hostname.split('.').splice(-2)).join('.')
+    : null
+	cookie.save(TOKEN_STORAGE_KEY, token, { domain, path: '/', maxAge })
 }
 
 export function deleteToken() {
