f97aac6abf7e50740f12fb89c94600e0cf1ff745
Michael Martin <martinmsm@gmail.com>
2018-04-20 23:14:41 -0500

    Use ENV vars

--- nodejs/start-server.js
+++ nodejs/start-server.js
@@ -4,8 +4,8 @@ const heapdump = require('heapdump')
 const server = require('./server.compiled')
 
 const agent = stackimpact.start({
-	agentKey: '1f784b84d9c81db6dd320f8fcc144ca01f271bf5',
-	appName: 'bible-nodejs',
+	agentKey: process.env.STACK_IMPACT_AGENT_KEY,
+	appName: process.env.NEW_RELIC_APP_NAME
 });
 
 server()
