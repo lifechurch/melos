372cf4c5e7a80428785ba782177adfec624f001e
Tommy George <tommy.george@youversion.com>
2018-05-31 16:57:02 -0500

    BAFK page for Portugal Portuguese pt-PT
    Ticket: https://jira.lifechurch.tv/browse/YV-16447

--- ruby/app/controllers/redirects_controller.rb
+++ ruby/app/controllers/redirects_controller.rb
@@ -1,6 +1,6 @@
 class RedirectsController < ApplicationController
 
-  before_filter :force_login, except: [:settings_notifications, :trending, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong, :kinderbybel, :pambatang, :thaibafk, :thieunhi, :ragazzi, :dzieci, :copii, :votd, :barn, :hk_kids, :lasten, :ua_kids, :paidia, :ben_kids, :barnebibelen, :huuhduud, :yezingane, :watoto, :hvvdvvd ]
+  before_filter :force_login, except: [:settings_notifications, :trending, :settings_vod_subscriptions, :lifechurchtv, :ninos, :ertong, :aideul, :criancas, :deti, :kinderen, :kinder, :enfants, :ar_kids, :wmf, :anak, :cocuk, :kodomo, :er_tong, :kinderbybel, :pambatang, :thaibafk, :thieunhi, :ragazzi, :dzieci, :copii, :votd, :barn, :hk_kids, :lasten, :ua_kids, :paidia, :ben_kids, :barnebibelen, :huuhduud, :yezingane, :watoto, :hvvdvvd, :criancas]
 
   prepend_before_filter :mobile_redirect, only: [:bookmarks, :profile, :friends, :notes, :badges, :highlights, :connections]
   # skip_filter :set_page,
@@ -224,9 +224,13 @@ class RedirectsController < ApplicationController
   def watoto
     redirect_to('/sw/kids')
   end
-  
+
   def hvvdvvd
-    redirect_to('/mn/kids')    
+    redirect_to('/mn/kids')
+  end
+
+  def criancas
+    redirect_to('/pt-PT/kids')
   end
 
 end
