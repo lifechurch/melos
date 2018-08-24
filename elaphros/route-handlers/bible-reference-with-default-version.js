f1e93362f2a186f23b84188ae1f0c58d3e763773
Michael Martin <martinmsm@gmail.com>
2018-08-24 11:51:35 -0500

    Canonicalize/Localize redirect URLs

--- elaphros/route-handlers/bible-reference-with-default-version.js
+++ elaphros/route-handlers/bible-reference-with-default-version.js
@@ -1,12 +1,14 @@
 const newrelic = require('../server/get-new-relic')()
+const seoUtils = require('../utils/seo')
 
 const DEFAULT_VERSION = process.env.BIBLE_DEFAULT_VERSION || 1
 const DEFAULT_USFM = process.env.BIBLE_DEFAULT_USFM || 'JHN.1'
+const DEFAULT_VERSION_ABBR = process.env.BIBLE_DEFAULT_VERSION_ABBR || 'KJV'
 
 module.exports = function bibleVersionWithDefaultReference(req, reply) {
   if (newrelic) {
     newrelic.setTransactionName('bible-reference-with-default-version')
   }
 
-  return reply.redirect(307, `/bible/${DEFAULT_VERSION}/${DEFAULT_USFM}`)
+  return reply.redirect(307, seoUtils.getCanonicalUrl('bible', `${DEFAULT_VERSION}`, `${DEFAULT_VERSION_ABBR}`, `${DEFAULT_USFM}`, req.detectedLng))
 }
--- elaphros/route-handlers/bible-version-with-default-reference.js
+++ elaphros/route-handlers/bible-version-with-default-reference.js
@@ -1,5 +1,6 @@
 const Raven = require('raven')
 const api = require('@youversion/js-api')
+const seoUtils = require('../utils/seo')
 const newrelic = require('../server/get-new-relic')()
 const validateApiResponse = require('../utils/validate-api-response')
 const getFirstUsfmForVersion = require('../utils/bible/get-first-usfm-for-version')
@@ -7,6 +8,7 @@ const getFirstUsfmForVersion = require('../utils/bible/get-first-usfm-for-versio
 const Bible = api.getClient('bible')
 const DEFAULT_VERSION = process.env.BIBLE_DEFAULT_VERSION || 1
 const DEFAULT_USFM = process.env.BIBLE_DEFAULT_USFM || 'JHN.1'
+const DEFAULT_VERSION_ABBR = process.env.BIBLE_DEFAULT_VERSION_ABBR || 'KJV'
 
 module.exports = function bibleVersionWithDefaultReference(req, reply) {
   if (newrelic) {
@@ -22,13 +24,13 @@ module.exports = function bibleVersionWithDefaultReference(req, reply) {
   versionPromise.then((version) => {
     if (!validateApiResponse(version)) {
       req.log.warn(`Invalid Bible version ${versionId}`)
-      return reply.redirect(303, `/bible/${DEFAULT_VERSION}/${DEFAULT_USFM}`)
+      return reply.redirect(303, seoUtils.getCanonicalUrl('bible', `${DEFAULT_VERSION}`, `${DEFAULT_VERSION_ABBR}`, `${DEFAULT_USFM}`, req.detectedLng))
     }
     const firstUsfm = getFirstUsfmForVersion(version, req.log)
-    return reply.redirect(301, `/bible/${versionId}/${firstUsfm}`)
+    return reply.redirect(301, seoUtils.getCanonicalUrl('bible', `${versionId}`, `${version.local_abbreviation}`, `${firstUsfm}`, req.detectedLng))
   }, (e) => {
     Raven.captureException(e)
     req.log.error(`Error getting Bible version ${e.toString}`)
-    return reply.redirect(307, `/bible/${DEFAULT_VERSION}/${DEFAULT_USFM}`)
+    return reply.redirect(307, seoUtils.getCanonicalUrl('bible', `${DEFAULT_VERSION}`, `${DEFAULT_VERSION_ABBR}`, `${DEFAULT_USFM}`, req.detectedLng))
   })
 }
