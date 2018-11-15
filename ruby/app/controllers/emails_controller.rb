dd2620791140de9ded41d2437b135e68eaad75a4
Ezrah Nyanat <ezrah.nyanat@life.church>
2018-11-15 11:33:17 -0600

    For testing

--- ruby/app/controllers/emails_controller.rb
+++ ruby/app/controllers/emails_controller.rb
@@ -18,11 +18,11 @@ class EmailsController < ApplicationController
   # Custom action to confirm an email change via a token provided in confirmation email sent to user
   def confirm_update
     @user = User.confirm_update_email(params[:token])
-    render action: "confirm_update"
-    # if @user.valid?
-    #   location = current_user ? user_email_path(current_user) : sign_in_path
-    #   redirect_to( location , notice: t('users.confirm update email success'))
-    # end
+    # render action: "confirm_update"
+    if @user.valid?
+      location = current_user ? user_email_path(current_user) : sign_in_path
+      redirect_to( location , notice: t('users.confirm update email success'))
+    end
   end
 
 end
