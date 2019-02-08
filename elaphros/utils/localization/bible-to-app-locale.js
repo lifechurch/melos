2c652aba63553cb2ad473d09958d8f104a88ba7d
Michael Martin <martinmsm@gmail.com>
2019-02-08 15:51:49 -0600

    Add try/catch handler for error when parsing locale and return first in list

--- elaphros/utils/localization/bible-to-app-locale.js
+++ elaphros/utils/localization/bible-to-app-locale.js
@@ -1,30 +1,35 @@
 const localeList = require('../../localization/locale-list.json')
 const getAppLocale = require('./get-app-locale')
 
-module.exports = function bibleToAppLocale({
-  name,
-  language_tag,
-  iso_639_1,
-  iso_639_3
-}) {
-  const matchingLocales = localeList.filter(({
-    englishName,
-    locale,
-    locale3,
-    other
-  }) => {
-    if (
-      language_tag === locale3 ||
-      iso_639_3 === locale3 ||
-      iso_639_1 === locale.replace('-', '_') ||
-      iso_639_3 === locale3.split('_')[0] ||
-      language_tag === other ||
-      name === englishName
-    ) {
-      return true
-    }
-    return false
-  })
-  if (matchingLocales.length === 0) return 'en'
-  return getAppLocale(matchingLocales.shift())
+module.exports = function bibleToAppLocale(params) {
+  try {
+    const {
+      name,
+      language_tag,
+      iso_639_1,
+      iso_639_3
+    } = params
+    const matchingLocales = localeList.filter(({
+      englishName,
+      locale,
+      locale3,
+      other
+    }) => {
+      if (
+        language_tag === locale3 ||
+        iso_639_3 === locale3 ||
+        iso_639_1 === locale.replace('-', '_') ||
+        iso_639_3 === locale3.split('_')[0] ||
+        language_tag === other ||
+        name === englishName
+      ) {
+        return true
+      }
+      return false
+    })
+    if (matchingLocales.length === 0) return 'en'
+    return getAppLocale(matchingLocales.shift())
+  } catch (e) {
+    return localeList[0]
+  }
 }
