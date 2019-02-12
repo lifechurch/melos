ec4be9bf8cc5cdea35f99996e1a303d4d15be27d
Michael Martin <martinmsm@gmail.com>
2019-02-12 12:32:39 -0600

    Adding custom selectors for lens settings

--- nodejs/packages/youversion-api-redux/src/endpoints/lens-settings/index.js
+++ nodejs/packages/youversion-api-redux/src/endpoints/lens-settings/index.js
@@ -2,6 +2,39 @@ import apiEndpoint from '../../generators/apiEndpoint'
 
 const endpoint = 'lens'
 
+export const unsubscribeStatus = (state) => {
+	const unsubscribe = state.lensSettings.unsubscribe
+
+	if (typeof unsubscribe !== 'object') {
+		return 'other'
+	}
+
+	if (unsubscribe.loading === true) {
+		return 'loading'
+	}
+
+	if (Array.isArray(unsubscribe.data.errors) && unsubscribe.data.errors.length > 0) {
+		return 'error'
+	}
+
+	if (typeof unsubscribe.data.notifications === 'object') {
+		return 'success'
+	}
+
+	return state
+}
+
+export const unsubscribeErrors = (state) => {
+	const unsubscribe = state.lensSettings.unsubscribe
+
+	if (typeof unsubscribe !== 'object' || !Array.isArray(unsubscribe.errors)) {
+		return []
+	}
+
+	return unsubscribe.errors
+}
+
+
 const methods = {
 	unsubscribe: {
 		url: '/4.0/settings%3Aunsubscribe_all',
