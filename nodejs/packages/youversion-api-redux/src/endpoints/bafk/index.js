096ac6e81c8348e2ebfaac030d7147bf53ad9a6c
Michael Martin <martinmsm@gmail.com>
2019-02-13 16:44:12 -0600

    Adding BAFK Unsubscribe to api-redux

---
+++ nodejs/packages/youversion-api-redux/src/endpoints/bafk/index.js
@@ -0,0 +1,46 @@
+import apiEndpoint from '../../generators/apiEndpoint'
+
+const endpoint = 'bafk'
+
+export const unsubscribeStatus = (state) => {
+	const unsubscribe = state.bafk.unsubscribe
+
+	if (typeof unsubscribe !== 'object') {
+		return 'other'
+	}
+
+	if (unsubscribe.loading === true) {
+		return 'loading'
+	}
+
+	if (Array.isArray(unsubscribe.data.errors) && unsubscribe.data.errors.length > 0) {
+		return 'error'
+	}
+
+	if (typeof unsubscribe.data.notifications === 'object') {
+		return 'success'
+	}
+
+	return 'loading'
+}
+
+export const unsubscribeErrors = (state) => {
+	const unsubscribe = state.bafk.unsubscribe
+
+	if (typeof unsubscribe !== 'object' || !Array.isArray(unsubscribe.errors)) {
+		return []
+	}
+
+	return unsubscribe.errors
+}
+
+
+const methods = {
+	unsubscribe: {
+		url: '/4.0/settings%3Aunsubscribe_all',
+	}
+}
+
+const bafkEndpoint = apiEndpoint(endpoint, methods)
+
+export default bafkEndpoint
