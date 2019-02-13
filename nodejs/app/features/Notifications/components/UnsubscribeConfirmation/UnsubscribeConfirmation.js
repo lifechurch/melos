c63481abe59d25b35289a0be27e8f76f015ddca4
Michael Martin <martinmsm@gmail.com>
2019-02-13 12:58:52 -0600

    Add support for Lens Unsubscribe

--- nodejs/app/features/Notifications/components/UnsubscribeConfirmation/UnsubscribeConfirmation.js
+++ nodejs/app/features/Notifications/components/UnsubscribeConfirmation/UnsubscribeConfirmation.js
@@ -10,137 +10,169 @@ import XMark from '../../../../components/XMark'
 import YouVersion from '../../../../components/YVLogo'
 
 class UnsubscribeConfirmation extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { ready: false }
-	}
+  constructor(props) {
+    super(props)
+    this.state = { ready: false }
+  }
 
-	componentDidMount() {
-		const { type } = this.props
-		this.setState({ ready: true })
-		this.unsubscribe(type)
-	}
+  componentDidMount() {
+    /* eslint-disable react/no-did-mount-set-state */
+    const { type } = this.props
+    this.setState({ ready: true })
+    this.unsubscribe(type)
+  }
 
-	componentWillReceiveProps(nextProps) {
-		const { type: nextType } = nextProps
-		const { type } = this.props
+  componentWillReceiveProps(nextProps) {
+    const { type: nextType } = nextProps
+    const { type } = this.props
 
-		if (type !== nextType) {
-			this.unsubscribe(nextType)
-		}
-	}
+    if (type !== nextType) {
+      this.unsubscribe(nextType)
+    }
+  }
 
-	unsubscribe = (type) => {
-		const {
-			loggedIn,
-			token,
-			data,
-			unsubscribe
-		} = this.props
+  getStatus = () => {
+    const {
+      status,
+      lensStatus,
+      product
+    } = this.props
 
-		if (type && (token || loggedIn)) {
-			unsubscribe({ type, token, data })
-		}
-	}
+    if (product === 'lens') return lensStatus
+    else return status
+  }
 
-	render() {
-		const {
-			token,
-			type,
-			status,
-			localizedLink,
-			errors,
-			intl,
+  getErrors = () => {
+    const {
+      errors,
+      lensErrors,
+      product
+    } = this.props
+
+    if (product === 'lens') return lensErrors
+    else return errors
+  }
+
+  unsubscribe = (type) => {
+    const {
+      loggedIn,
+      token,
+      data,
+      unsubscribe
+    } = this.props
+
+    if (type && (token || loggedIn)) {
+      unsubscribe({ type, token, data })
+    }
+  }
+
+  render() {
+    const {
+      token,
+      type,
+      localizedLink,
+      intl,
       plan,
-			tokenIdentity: {
-				languageTag
-			}
-		} = this.props
+      product,
+      tokenIdentity: {
+        languageTag
+      }
+    } = this.props
 
-		const { ready } = this.state
+    const status = this.getStatus()
+    const errors = this.getErrors()
 
-		let statusMark
-		if (status === 'success') {
-			statusMark = <CheckMark width={28} height={21} />
-		} else if (status === 'loading') {
-			statusMark = <CheckMark width={28} height={21} fill="#777" />
-		} else {
-			statusMark = <XMark width={21} height={21} fill="#F00" />
-		}
+    const { ready } = this.state
 
-		let statusText
-		let tokenValid = !!token
-		if (['success', 'loading'].indexOf(status) > -1) {
-			const plan_title = (plan && typeof plan === 'object') ? (plan.name[languageTag] || plan.name.default) : ''
-			statusText = <FormattedHTMLMessage id={`unsubscribe.success.${type}`} values={{ plan_title }} />
-		} else {
-			let errorType = 'other_type'
-			errors.forEach((e) => {
-				if (e.key === 'users.user_verification_token.invalid') {
-					errorType = 'token'
-					tokenValid = false
-				}
-			})
-			statusText = <FormattedMessage id={`unsubscribe.error.${errorType}`} />
-		}
+    let statusMark
+    if (status === 'success') {
+      statusMark = <CheckMark width={28} height={21} />
+    } else if (status === 'loading') {
+      statusMark = <CheckMark width={28} height={21} fill="#777" />
+    } else {
+      statusMark = <XMark width={21} height={21} fill="#F00" />
+    }
 
-		return (
-			<div className="yv-unsubscribe-confirmation">
-				<Helmet title={intl.formatMessage({ id: 'unsubscribe.links.manage' })} />
-				<div className="card-wrapper">
-					<a href="/"><YouVersion height={21} width={150} /></a>
-					<Card>
-						<div className={status === 'loading' ? 'ghost' : ''}>
-							{statusMark}
-							<p>{statusText}</p>
-							<Link to={localizedLink(`/unsubscribe/manage${ready && tokenValid ? `?token=${token}` : ''}`, languageTag)} className="solid-button"><FormattedMessage id="unsubscribe.links.manage" /></Link>
-							{ type !== 'all' && <Link to={localizedLink(`/unsubscribe?type=all${ready && tokenValid ? `&token=${token}` : ''}`, languageTag)}><FormattedMessage id="unsubscribe.links.unsub all" /></Link>}
-						</div>
-					</Card>
-				</div>
-			</div>
-		)
-	}
+    let statusText
+    let tokenValid = !!token
+    if (['success', 'loading'].indexOf(status) > -1) {
+      const plan_title = (plan && typeof plan === 'object') ? (plan.name[languageTag] || plan.name.default) : ''
+      statusText = <FormattedHTMLMessage id={`unsubscribe.success.${type}`} values={{ plan_title }} />
+    } else {
+      let errorType = 'other_type'
+      errors.forEach((e) => {
+        if (e.key === 'users.user_verification_token.invalid') {
+          errorType = 'token'
+          tokenValid = false
+        }
+      })
+      statusText = <FormattedMessage id={`unsubscribe.error.${errorType}`} />
+    }
+
+    return (
+      <div className="yv-unsubscribe-confirmation">
+        <Helmet title={intl.formatMessage({ id: 'unsubscribe.links.manage' })} />
+        <div className="card-wrapper">
+          <a href="/"><YouVersion height={21} width={150} /></a>
+          <Card>
+            <div className={status === 'loading' ? 'ghost' : ''}>
+              {statusMark}
+              <p>{statusText}</p>
+
+              { product !== 'lens' && <Link to={localizedLink(`/unsubscribe/manage${ready && tokenValid ? `?token=${token}` : ''}`, languageTag)} className="solid-button"><FormattedMessage id="unsubscribe.links.manage" /></Link>}
+              { (type !== 'all' && product !== 'lens') && <Link to={localizedLink(`/unsubscribe?type=all${ready && tokenValid ? `&token=${token}` : ''}`, languageTag)}><FormattedMessage id="unsubscribe.links.unsub all" /></Link>}
+            </div>
+          </Card>
+        </div>
+      </div>
+    )
+  }
 }
 
 UnsubscribeConfirmation.propTypes = {
-	type: PropTypes.oneOf([
-		'badges',
-		'comments',
-		'friendships',
-		'likes',
-		'moments',
-		'newsletter',
-		'pwf_accepts',
-		'pwf_comments',
-		'reading_plans',
-		'rp_daily',
-		'votd',
-		'votd_image',
-		'contact_joins',
-		'all'
-	]),
-	token: PropTypes.string,
-	data: PropTypes.string,
-	status: PropTypes.oneOf([ 'loading', 'success', 'other', 'error' ]),
-	errors: PropTypes.array,
-	loggedIn: PropTypes.bool,
-	localizedLink: PropTypes.func.isRequired,
-	unsubscribe: PropTypes.func.isRequired,
-	intl: PropTypes.object.isRequired,
-	tokenIdentity: PropTypes.object,
-	plan: PropTypes.object
+  type: PropTypes.oneOf([
+    'badges',
+    'comments',
+    'friendships',
+    'likes',
+    'moments',
+    'newsletter',
+    'pwf_accepts',
+    'pwf_comments',
+    'reading_plans',
+    'rp_daily',
+    'votd',
+    'votd_image',
+    'contact_joins',
+    'all'
+  ]),
+  token: PropTypes.string,
+  data: PropTypes.string,
+  status: PropTypes.oneOf([ 'loading', 'success', 'other', 'error' ]),
+  errors: PropTypes.array,
+  lensStatus: PropTypes.oneOf([ 'loading', 'success', 'other', 'error' ]),
+  lensErrors: PropTypes.array,
+  loggedIn: PropTypes.bool,
+  localizedLink: PropTypes.func.isRequired,
+  unsubscribe: PropTypes.func.isRequired,
+  intl: PropTypes.object.isRequired,
+  tokenIdentity: PropTypes.object,
+  plan: PropTypes.object,
+  product: PropTypes.object
 }
 
 UnsubscribeConfirmation.defaultProps = {
-	type: null,
-	token: null,
-	data: null,
-	status: null,
-	loggedIn: false,
-	errors: [],
-	tokenIdentity: {},
-	plan: null
+  type: null,
+  token: null,
+  data: null,
+  status: 'loading',
+  lensStatus: 'loading',
+  loggedIn: false,
+  errors: [],
+  lensErrors: [],
+  tokenIdentity: {},
+  plan: null,
+  product: null
 }
 
 export default UnsubscribeConfirmation
--- nodejs/app/standalone/Unsubscribe/reducer.js
+++ nodejs/app/standalone/Unsubscribe/reducer.js
@@ -3,33 +3,35 @@ import { routerReducer } from 'react-router-redux'
 import notifications from '@youversion/api-redux/lib/endpoints/notifications/reducer'
 import users from '@youversion/api-redux/lib/endpoints/users/reducer'
 import readingPlans from '@youversion/api-redux/lib/endpoints/readingPlans/reducer'
+import lensSettings from '@youversion/api-redux/lib/endpoints/lens-settings'
 
 const emptyReducer = (state = {}) => { return state }
 
 const rootReducer = combineReducers({
-	auth: emptyReducer,
-	bibleReader: emptyReducer,
-	eventFeeds: emptyReducer,
-	content: emptyReducer,
-	event: emptyReducer,
-	modals: emptyReducer,
-	loc: emptyReducer,
-	locations: emptyReducer,
-	plans: emptyReducer,
-	plansDiscovery: emptyReducer,
-	configuration: emptyReducer,
-	references: emptyReducer,
-	routing: routerReducer,
-	serverLanguageTag: emptyReducer,
-	altVersions: emptyReducer,
-	hosts: emptyReducer,
-	passage: emptyReducer,
-	locale: emptyReducer,
-	api: combineReducers({
-		notifications,
-		users,
-		readingPlans
-	})
+  auth: emptyReducer,
+  bibleReader: emptyReducer,
+  eventFeeds: emptyReducer,
+  content: emptyReducer,
+  event: emptyReducer,
+  modals: emptyReducer,
+  loc: emptyReducer,
+  locations: emptyReducer,
+  plans: emptyReducer,
+  plansDiscovery: emptyReducer,
+  configuration: emptyReducer,
+  references: emptyReducer,
+  routing: routerReducer,
+  serverLanguageTag: emptyReducer,
+  altVersions: emptyReducer,
+  hosts: emptyReducer,
+  passage: emptyReducer,
+  locale: emptyReducer,
+  lensSettings: combineReducers(lensSettings.reducers),
+  api: combineReducers({
+    notifications,
+    users,
+    readingPlans
+  })
 })
 
 export default rootReducer
