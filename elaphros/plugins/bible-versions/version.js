b10549b84834f39ca7e1f6e9839a5d9061fcfc35
Michael Martin <michael.martin@life.church>
2019-04-10 11:54:03 -0500

    Forgot about the canonical URL

--- elaphros/plugins/bible-versions/version.js
+++ elaphros/plugins/bible-versions/version.js
@@ -7,9 +7,9 @@ const getFirstUsfmForVersion = require('../../utils/bible/get-first-usfm-for-ver
 
 const Bible = api.getClient('Bible')
 
-module.exports = function BibleVersions(req, reply) {
+module.exports = function BibleVersion(req, reply) {
   if (reply.newrelic) {
-    reply.newrelic.setTransactionName('bible-versions')
+    reply.newrelic.setTransactionName('bible-version')
   }
 
   const { host } = req.urlData()
@@ -23,24 +23,28 @@ module.exports = function BibleVersions(req, reply) {
 
   const allPromises = Promise.all([ versionPromise ])
 
-  const canonicalPath = getLocalizedLink('/versions', req.detectedLng)
-  const canonicalUrl = `${host ? `https://${host}` : ''}${canonicalPath}`
+  allPromises.then(([ version ]) => {
+    const canonicalPath = seoUtils.getCanonicalUrl('bible-version', version.id, version.title, version.abbreviation, req.detectedLng)
+    const canonicalUrl = `${host ? `https://${host}` : ''}${canonicalPath}`
 
-  return reply.view('/ui/pages/bible-versions/version.marko', {
-    req,
-    getLocalizedLink,
-    allPromises,
-    getFirstUsfmForVersion,
-    $global: {
-      __: reply.res.__,
-      __mf: reply.res.__mf,
-      locale: req.detectedLng,
-      seoUtils,
-      bibleToAppLocale,
-      textDirection: 'ltr',
-      localeDetails: getAppLocaleDetails(req.detectedLng),
-      canonicalUrl,
-      canonicalPath,
-    }
+    return reply.view('/ui/pages/bible-versions/version.marko', {
+      req,
+      getLocalizedLink,
+      allPromises,
+      getFirstUsfmForVersion,
+      $global: {
+        __: reply.res.__,
+        __mf: reply.res.__mf,
+        locale: req.detectedLng,
+        seoUtils,
+        bibleToAppLocale,
+        textDirection: 'ltr',
+        localeDetails: getAppLocaleDetails(req.detectedLng),
+        canonicalUrl,
+        canonicalPath,
+      }
+    })
+  }, (e) => {
+    reply.captureException(e, 'Error getting Bible Version')
   })
 }
